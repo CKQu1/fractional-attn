75a8e1394fd42b1fa52a1e09c3d2f4ddc327ab11
CKQu1 <kevinqu1995@hotmail.com>
2023-10-18 18:40:54 +1100

    use new singularity container, attempt to use DDP

--- PBS_script/frac_train_imdb.sh
+++ PBS_script/frac_train_imdb.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #PBS -P frac_attn
-##PBS -l select=1:ncpus=0:ngpus=1:mem=16gb
-#PBS -l select=1:ncpus=4:ngpus=0:mem=40gb
+##PBS -l select=1:ncpus=0:ngpus=1:mem=24gb
+##PBS -l select=1:ncpus=4:ngpus=0:mem=16gb
+#PBS -l select=1:ncpus=4:ngpus=0:mem=12gb
 ##PBS -l walltime=2:00:00
+##PBS -l walltime=47:59:59
 #PBS -l walltime=23:59:59
 #PBS -e PBSout/
 #PBS -o PBSout/
@@ -10,5 +12,8 @@
 module load singularity
 PBS_O_WORKDIR="/project/frac_attn/fractional-attn"
 cd ${PBS_O_WORKDIR}
-singularity exec ../built_containers/FaContainer.sif python frac_train_classification_imdb.py
 
+# check if relevant modules exist
+#singularity exec --home ${PBS_O_WORKDIR}  ../built_containers/FaContainer.sif python -c "import torch, transformers, dgl; print([dgl.__version__, transformers.__version__, torch.__version__,torch.cuda.is_available()])"
+
+singularity exec --home ${PBS_O_WORKDIR} ../built_containers/FaContainer.sif python frac_train_classification_imdb.py

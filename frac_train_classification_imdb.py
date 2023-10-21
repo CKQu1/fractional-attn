687b2619ef6a7f4c126575cb9fc58208b5c70f9c
CKQu1 <kevinqu1995@hotmail.com>
2023-10-21 16:02:59 +1100

    updated to same setting as train_classification_imdb.py

--- frac_train_classification_imdb.py
+++ frac_train_classification_imdb.py
@@ -1,10 +1,9 @@
+import numpy as np
 import os
 import pandas as pd
 import uuid
-import sys
 import torch
 from time import time
-sys.path.append(f'{os.getcwd()}')
 from path_setup import droot
 
 from os.path import join
@@ -16,9 +15,6 @@ from datasets import load_dataset,load_metric,load_from_disk
 from models.diffuser_app import DiffuserForSequenceClassification
 from models.diffuser_utils import DiffuserConfig
 from graphtrainer import graphTrainer
-
-dev = torch.device(f"cuda:{torch.cuda.device_count()-1}"
-                   if torch.cuda.is_available() else "cpu")
 
 logging.set_verbosity_debug()
 logger = logging.get_logger()
@@ -41,7 +37,7 @@ dataset_dir = join(droot, "DATASETS")
 if not os.path.isdir(dataset_dir): os.makedirs(dataset_dir)
 
 imdb = load_dataset("imdb", cache_dir=dataset_dir)
-# tokenizer = RobertaTokenizer.from_pretrained("./roberta-tokenizer", max_length = 1024)
+#tokenizer = RobertaTokenizer.from_pretrained("./roberta-tokenizer", max_length = 1024)
 tokenizer = RobertaTokenizer(tokenizer_file = "./roberta-tokenizer/tokenizer.json",
                              vocab_file     = "./roberta-tokenizer/vocab.json",
                              merges_file    = "./roberta-tokenizer/merges.txt",
@@ -60,44 +56,63 @@ else:
     tokenized_imdb = load_from_disk(dataset_dir)
 
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
 #config = DiffuserConfig.from_json_file("./models/config.json")
 config = DiffuserConfig.from_json_file("./models/config_simple.json")
-config.num_labels = 2
-with_frac = False
+config.num_labels = 2``
+with_frac = True
+gamma = 0.5
+attn_setup = {"with_frac":with_frac, "gamma":gamma}
+model = DiffuserForSequenceClassification(config, **attn_setup)
 
-attn_setup = {"with_frac":True, "gamma":0.5}
-#model =  DiffuserForSequenceClassification(config = config).cuda()
-model =  DiffuserForSequenceClassification(config, **attn_setup).to(dev)
+train_with_ddp = True
+train_with_ddp = torch.distributed.is_available() and train_with_ddp
+
+dev = torch.device(f"cuda:{torch.cuda.device_count()-1}"
+                   if torch.cuda.is_available() else "cpu")    
+model = model.to(dev)
 
 uuid_ = str(uuid.uuid4())[:8]
-model_dir = join(droot, "save_imdb_frac", uuid_)
+if with_frac:
+    model_dir = join(droot, "trained_models", f"save_imdb_diffuser_test", uuid_)
+else:
+    model_dir = join(droot, "trained_models", f"save_imdb_frac_diffuser_test", uuid_)
+if not os.path.isdir(model_dir): os.makedirs(model_dir)
 training_args = TrainingArguments(
     output_dir = model_dir,
     learning_rate = 3e-5,
     per_device_train_batch_size = 2,
     per_device_eval_batch_size = 2,
-    #num_train_epochs = 1,
-    #num_train_epochs = 0.0025,
-    num_train_epochs = 0.00125,
+    #num_train_epochs = 20,
+    num_train_epochs = 1,
+    max_steps = 10,
     weight_decay = 0.01,
     evaluation_strategy = "steps",
-    eval_steps = 0.25,
+    #eval_steps = 2000,
+    eval_steps = 5,
+    logging_strategy="steps",
     #logging_steps = 500,
+    logging_steps = 5,
     #save_steps = 500,
-    logging_steps = 0.2,
-    save_steps = 0.2,    
+    save_steps = 5,  
     seed = 42,
-    #warmup_steps = 50,
     warmup_steps = 2,
-    gradient_accumulation_steps = 8,
-    prediction_loss_only=True
+    gradient_accumulation_steps = 8
+    #prediction_loss_only=False
 )
 
 if dev.type != "cpu":
-    steps_per_train_epoch       = int(len(tokenized_imdb['train'])/(training_args.per_device_train_batch_size*torch.cuda.device_count()*training_args.gradient_accumulation_steps ))
+    device_name, device_total = "GPU", torch.cuda.device_count()
 else:
-    steps_per_train_epoch       = int(len(tokenized_imdb['train'])/(training_args.per_device_train_batch_size*torch.get_num_threads()*training_args.gradient_accumulation_steps ))
+    device_name, device_total = "CPU", torch.get_num_threads()
+
+if not train_with_ddp:
+    device_total = 1
+# this is assuming DDP is being deployed?
+steps_per_train_epoch = int(len(tokenized_imdb['train'])/(training_args.per_device_train_batch_size*device_total*training_args.gradient_accumulation_steps ))
+print(f"per_device_train_batch_size: {training_args.per_device_train_batch_size}")
+print(f"{device_name} count: {device_total}")
+print(f"gradient_accumulation_steps: {training_args.gradient_accumulation_steps} \n")    
+
 training_args.eval_steps    = int(steps_per_train_epoch)
 training_args.logging_steps = int(steps_per_train_epoch/5)
 training_args.save_steps    = int(steps_per_train_epoch)

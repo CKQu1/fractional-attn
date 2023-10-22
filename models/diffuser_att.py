31713841a368384bfa9b5ec55d59a8d2f5fda313
CKQu1 <kevinqu1995@hotmail.com>
2023-10-23 01:21:07 +1100

    apply sparsification to LimitFracSelfAttention from graphtrainer.py, added comments for potential dropout app

--- models/diffuser_att.py
+++ models/diffuser_att.py
@@ -1,6 +1,7 @@
 import torch
 import math
 import dgl.function as fn
+import dgl.ops as ops
 import numpy as np
 import operator as operator
 from functools import reduce
@@ -36,9 +37,11 @@ class DiffuserAttention(nn.Module):
         if not self.with_frac:
             self.self = DiffuserSelfAttention(config, layer_id)
         else:
-            assert 0 < kwargs.get('gamma') < 1, "gamma for FracSelfAttention is ill-defined!"         
+            error_message = f"gamma = {kwargs.get('gamma')} with type {type(kwargs.get('gamma'))} for FracSelfAttention is ill-defined!"
+            assert 0 < kwargs.get('gamma') < 1, error_message
             gamma = kwargs.get('gamma')
-            self.self = LimitFracSelfAttention(config, layer_id, gamma)
+            #self.self = LimitFracSelfAttention(config, layer_id, gamma)
+            self.self = LimitFracSelfAttention_dgl(config, layer_id, gamma)
         self.output = DiffuserSelfOutput(config)
 
     def forward(
@@ -311,7 +314,7 @@ class DiffuserFracSelfAttention(nn.Module):
         return outputs + (global_attn_probs,) if (is_global_attn and output_attentions) else outputs
 """
 
-# Fractional diffusion (unnormalized fractional Laplacian)
+# Limiting fractional diffusion (unnormalized fractional Laplacian)
 class LimitFracSelfAttention(nn.Module):
     def __init__(self, config, layer_id, gamma):
         super().__init__()
@@ -378,31 +381,163 @@ class LimitFracSelfAttention(nn.Module):
         
         Bmat = torch.bmm( key_vectors.transpose(0,1), query_vectors.transpose(0,1).transpose(1,2) )  # KQ product: H,BN,BN
         Bmat = Bmat.exp()  # no softmax applied, consistent with Diffuser
-        bool_mask = (attention_mask>0).reshape(-1).unsqueeze(-1)
+        #Bmat = nn.functional.dropout(Bmat, p=self.dropout, training=self.training)
+
+        # attn sparsification
+        Bmat.masked_fill_((g>0)==False, 0)
+        # alternative brute force way
+        #with torch.no_grad():
+        #   Bmat *= g
+
+        bool_mask = (attention_mask>=0).reshape(-1).unsqueeze(-1)
         Bmat.masked_fill_(bool_mask==False, 0)  # attention mask
         BN = seq_len * batch_size    
         out_degree = Bmat.sum(axis=2)
         rhos = out_degree.max(axis=1).values 
         with torch.no_grad():   
             Bmat -= torch.diag_embed(out_degree)
-            Bmat += torch.diag_embed(rhos.unsqueeze(-1).repeat(1, BN))
+            Bmat += torch.diag_embed(rhos.unsqueeze(-1).repeat(1, BN))  # apply dropout after this?
         B_power = torch.eye(BN, requires_grad=True).reshape(1, BN, BN).repeat(self.num_heads, 1, 1)
         L_gamma = torch.eye(BN, requires_grad=True).reshape(1, BN, BN).repeat(self.num_heads, 1, 1)        
 
-        N_approx = 2    
+        N_approx = 6    
         numerator, denominator = 1, 1
+        #error_tolerence = 1e-5  # should this be introduced?
         with torch.no_grad():
             for ii in range(1, N_approx+1):
                 numerator *= (self.gamma - ii + 1) * (-1)
                 denominator *= ii * rhos
                 coef = numerator/denominator            
-                B_power = torch.bmm(B_power, Bmat)            
+                B_power = torch.bmm(B_power, Bmat)  # apply dropout after this?          
                 L_gamma += coef.unsqueeze(-1).unsqueeze(-1) * B_power
             L_gamma *= rhos.unsqueeze(-1).unsqueeze(-1)**self.gamma
 
         L_gamma_diags = torch.diagonal( L_gamma, dim1=-2, dim2=-1 )
-        L_gamma_diags = L_gamma_diags / L_gamma_diags.sum(axis=1).unsqueeze(dim=-1)  # apply dropout after this?
-        s_dist = L_gamma_diags.unsqueeze(dim=-1).repeat(1,1,BN)    
+        L_gamma_diags = L_gamma_diags / L_gamma_diags.sum(axis=1).unsqueeze(dim=-1)  # apply dropout after this?     
+        s_dist = L_gamma_diags.unsqueeze(dim=-2).repeat(1,BN,1)  
+        #s_dist = nn.functional.dropout(s_dist, p=self.dropout, training=self.training)   
+        attn_output = torch.bmm(s_dist, value_vectors.transpose(0,1))
+        #attn_output = nn.functional.dropout(attn_output, p=self.dropout, training=self.training)
+
+        attn_output = attn_output.reshape(batch_size, seq_len,  self.num_heads, self.head_dim) # B,N,H,D        
+        assert attn_output.size() == (batch_size, seq_len, self.num_heads, self.head_dim), "Unexpected size"
+        attn_output = attn_output.transpose(0, 1).reshape(seq_len, batch_size, embed_dim).contiguous()
+        outputs = (attn_output.transpose(0, 1),) # Seq,B,D
+
+        return outputs + (global_attn_probs,) if (is_global_attn and output_attentions) else outputs
+
+
+# Limiting fractional diffusion with dgl (the same as above)
+class LimitFracSelfAttention_dgl(nn.Module):
+    def __init__(self, config, layer_id, gamma):
+        super().__init__()
+        if config.hidden_size % config.num_attention_heads != 0:
+            raise ValueError(
+                f"The hidden size ({config.hidden_size}) is not a multiple of the number of attention "
+                f"heads ({config.num_attention_heads})"
+            )
+        self.num_heads = config.num_attention_heads
+        self.head_dim = int(config.hidden_size / config.num_attention_heads)
+        self.embed_dim = config.hidden_size
+
+        self.query = nn.Linear(config.hidden_size, self.embed_dim)
+        self.key = nn.Linear(config.hidden_size, self.embed_dim)
+        self.value = nn.Linear(config.hidden_size, self.embed_dim)
+
+        self.dropout = config.attention_probs_dropout_prob
+
+        self.gamma = gamma
+
+        self.layer_id = layer_id
+        attention_window = config.attention_window[self.layer_id]
+        assert (
+            attention_window % 2 == 0
+        ), f"`attention_window` for layer {self.layer_id} has to be an even value. Given {attention_window}"
+        assert (
+            attention_window > 0
+        ), f"`attention_window` for layer {self.layer_id} has to be positive. Given {attention_window}"
+
+    def forward(
+        self,
+        hidden_states,
+        g=None,
+        attention_mask=None,
+        layer_head_mask=None,
+        is_index_masked=None,
+        is_index_global_attn=None,
+        is_global_attn=None,
+        output_attentions=False,
+    ):
+
+        hidden_states = hidden_states.transpose(0, 1) #(N,B,HD)
+        # attention_mask (B,N)
+        # project hidden states
+        query_vectors = self.query(hidden_states)
+        key_vectors = self.key(hidden_states)
+        value_vectors = self.value(hidden_states)   # (N,B,HD)
+
+        seq_len, batch_size, embed_dim = hidden_states.size()
+        assert (
+            embed_dim == self.embed_dim
+        ), f"hidden_states should have embed_dim = {self.embed_dim}, but has {embed_dim}" 
+
+        # normalize query
+        query_vectors /= math.sqrt(self.head_dim)
+
+        query_vectors = query_vectors.view(seq_len, batch_size, self.num_heads, self.head_dim).transpose(0, 1) # (B,N,H,D)
+        key_vectors = key_vectors.view(seq_len, batch_size, self.num_heads, self.head_dim).transpose(0, 1) # (B,N,H,D)
+        value_vectors = value_vectors.view(seq_len, batch_size, self.num_heads, self.head_dim).transpose(0, 1) #B,N,H,D
+
+        bool_mask = (attention_mask>=0 )
+        g = g.local_var()
+        g.ndata["mask"] = bool_mask.reshape(-1).unsqueeze(-1)        #BN,1
+        g.ndata['q'] =  query_vectors.reshape(-1, self.num_heads, self.head_dim) #BN,H,D
+        g.ndata['k'] =  key_vectors.reshape(-1, self.num_heads, self.head_dim) #BN,H,D
+        #g.ndata['v'] =  value_vectors.reshape(-1, self.num_heads, self.head_dim) #BN,H,D
+
+        g.apply_edges(fn.u_dot_v('k', 'q', 'score'))   #score: [E,H,1]    
+        g.edata['score'] = g.edata['score'].exp()  # exponential taken here, edge weights are non-neg
+        g.apply_edges(frac_mask_attention_score)   #kq
+        # creating Bmat in the format of ndata and store in 'score'   
+        BN = batch_size * seq_len
+
+        rev = reverse(g)
+        out_degree = ops.copy_e_sum(rev, g.edata['score'])  # out deg (verified!)
+        rhos = torch.max(out_degree, axis=0).values    
+
+        # pass score into B and modify edge weights
+        g.edata['B'] = g.edata.pop('score')  # score is popped into B for saving space
+        diag_count = 0
+        src, dst = g.edges()
+        num_nodes, num_edges = g.num_nodes(), g.num_edges()    
+        for eidx in range(num_edges):  # there should be a more efficient message passing method
+            src_node, dst_node = src[eidx], dst[eidx]
+            if src_node == dst_node:
+                g.edata['B'][eidx] += rhos - out_degree[diag_count]
+                diag_count += 1    
+
+        # initialization of B^n and L^alpha
+        g.ndata['B_power'] = torch.eye(BN, requires_grad=True).reshape(1, BN, BN).repeat(self.num_heads, 1, 1).transpose(0,1)  # stacked identity matrix tranposed
+        g.ndata['L_gamma'] = torch.eye(BN, requires_grad=True).reshape(1, BN, BN).repeat(self.num_heads, 1, 1).transpose(0,1)  # for storing fractional Laplacian
+
+        # trick for efficiently taking powers of Bmat (need to make sure the order of transpose is correct)    
+        N_approx = 6    
+        numerator, denominator = 1, 1
+        for ii in range(1, N_approx+1):
+            numerator *= (self.gamma - ii + 1) * (-1)
+            denominator *= ii * rhos
+            coef = numerator/denominator            
+            #print(coef)  # delete
+            g.update_all(fn.u_mul_e('B_power', 'B', 'm'), fn.sum('m', 'B_power'))
+            g.apply_nodes(lambda nodes: {'L_gamma' : nodes.data['L_gamma'] + coef * nodes.data['B_power']})
+            #g.ndata['L_gamma']= nn.functional.dropout(g.ndata['L_gamma'], p=p_dropout, training=training_dropout)    
+        g.apply_nodes(lambda nodes: {'L_gamma' : rhos**self.gamma * nodes.data['L_gamma']})
+
+        L_gamma_diags = torch.diagonal( g.ndata['L_gamma'].transpose(0,1), dim1=-2, dim2=-1 )
+        L_gamma_diags = L_gamma_diags / L_gamma_diags.sum(axis=1).unsqueeze(dim=-1)  # apply dropout after this?     
+        s_dist = L_gamma_diags.unsqueeze(dim=-2).repeat(1,BN,1)  
+        #s_dist = nn.functional.dropout(s_dist, p=self.dropout, training=self.training)   
+        value_vectors = value_vectors.reshape(-1, self.num_heads, self.head_dim)
         attn_output = torch.bmm(s_dist, value_vectors.transpose(0,1))
 
         attn_output = attn_output.reshape(batch_size, seq_len,  self.num_heads, self.head_dim) # B,N,H,D        

b630252e9a89fe51848285cd7c93ec9fb251faf2
CKQu1 <kevinqu1995@hotmail.com>
2023-10-28 22:43:46 +1100

    allow different datasets + models (still needs work); clean up variables

--- main_seq_classification.py
+++ main_seq_classification.py
@@ -6,7 +6,7 @@ import torch
 from time import time, sleep
 from path_setup import droot
 
-from os.path import join
+from os.path import join, isdir
 from sklearn.metrics import f1_score
 from transformers import TrainingArguments, DataCollatorWithPadding
 from transformers import RobertaTokenizer
@@ -37,8 +37,45 @@ def convert_train_history(ls):
     df_model = df_model.append(cur_dict, ignore_index=True)
     return df_model
 
-# debug model
-#python -i main_seq_classification --max_steps=1 --logging_steps=1 --save_steps=1 --eval_steps=1 --warmup_steps=0 --gradient_accumulation_steps=1 
+# simple wrapper for load_dataset
+def get_dataset(dataset_name, cache_dir):
+    if args.dataset_name in ['imdb', 'emotion', 'rotten_tomatoes']:
+        dataset = load_dataset(dataset_name, cache_dir=cache_dir)
+    elif args.dataset_name == "hyperpartisan_news_detection":
+        dataset = load_dataset(dataset_name, "byarticle", cache_dir=cache_dir)
+    elif args.dataset_name == "newsgroup":
+        dataset = load_dataset(dataset_name, '18828_alt.atheism', cache_dir=cache_dir)   
+    elif args.dataset_name == "cola":
+        dataset = load_dataset('glue', 'cola', cache_dir=cache_dir) 
+    else:
+        raise NameError(f"{dataset_name} not applicable!")
+    return dataset
+
+def get_dataset_cols(dataset):
+    return list(dataset.column_names.values())[0]
+
+# remove and rename key names in dataset
+def process_dataset_cols(dataset):
+    column_names = get_dataset_cols(dataset)
+    if 'hyperpartisan' in column_names:
+        dataset = dataset.rename_column("hyperpartisan", "label")
+    if 'sentence' in column_names:
+        dataset = dataset.rename_column('sentence', 'text')
+    
+    # for removing unused features
+    column_names = list(dataset.column_names.values())[0]
+    remove_columns = list(set(column_names) - {'input_ids', 'attention_mask', 'label'})
+    if len(remove_columns) > 0:
+        dataset = dataset.map(remove_columns=remove_columns)
+    
+    return dataset
+
+# quick run
+"""
+python -i main_seq_classification.py --max_steps=1 --logging_steps=1 --save_steps=1 --eval_steps=1\
+ --divider=500 --warmup_steps=0 --gradient_accumulation_steps=1 --dataset_name=cola\
+ --model_dir=droot/debug_mode8/model_0
+"""
 if __name__ == '__main__':
 
     # Training options
@@ -65,39 +102,53 @@ if __name__ == '__main__':
     parser.add_argument('--max_length', default=1024, type=int)
     parser.add_argument('--model_dir', default=None, type=str)
     # Dataset settings
-    parser.add_argument('--dataset_name', default='imdb', type=str)    
+    parser.add_argument('--dataset_name', default='imdb', type=str)
+    parser.add_argument('--divider', default=5, type=int)  # downsizing the test dataset
 
     args = parser.parse_args()    
 
+    repo_dir = os.getcwd()  # main dir 
     dev = torch.device(f"cuda:{torch.cuda.device_count()}"
-                    if torch.cuda.is_available() else "cpu")      
-
+                       if torch.cuda.is_available() else "cpu")   
+    device_name = "GPU" if dev.type != "cpu" else "CPU"
     train_with_ddp = torch.distributed.is_available() and args.train_with_ddp
     global_rank = None
     if train_with_ddp:
-        world_size =int(os.environ["WORLD_SIZE"])
+        world_size = int(os.environ["WORLD_SIZE"])
         global_rank = int(os.environ["RANK"])
         print(f"global_rank: {global_rank}")    
         print(f"Device in use: {dev}.")
-
-    repo_dir = os.getcwd()  # main dir    
+        device_total = world_size
+        #backend = "gloo" if dev.type != "cpu" else "nccl"
+        #import torch.distributed as dist
+        #dist.init_process_group(backend=backend)        
+    else:
+        device_total = 1       
 
     logging.set_verbosity_debug()
     logger = logging.get_logger()
 
+    # ---------------------------------------- 1. Dataset setup ----------------------------------------
+
     # should max_length also be added to parser args?
-    max_length = args.max_length
+    max_length = args.max_length    
     def preprocess_function(examples):
         return tokenizer(examples['text'], padding='max_length', truncation=True, max_length=max_length)
-    
+
     def preprocess_logits_for_metrics(logits, labels):
         preds = logits.argmax(dim=-1)
         return preds
         
-    metric_acc = load_metric("accuracy")
-    metric_f1 = load_metric("f1")
-    #metric_prcn = load_metric("precision") 
-    #metric_recall = load_metric("recall")       
+    if args.dataset_name == 'imdb':
+        metric_acc = load_metric("accuracy")
+        metric_f1 = load_metric("f1")
+        #metric_prcn = load_metric("precision") 
+        #metric_recall = load_metric("recall") 
+    else:
+        metric_acc = load_metric("accuracy", average='micro')
+        metric_f1 = load_metric("f1", average='micro')
+        #metric_prcn = load_metric("precision") 
+        #metric_recall = load_metric("recall")         
     def compute_metrics(eval_pred):
         preds, labels = eval_pred
         acc = metric_acc.compute(predictions=preds, references=labels)        
@@ -106,46 +157,66 @@ if __name__ == '__main__':
         #recall = metric_recall.compute(predictions=preds, references=labels)                
         #return {"accuracy":acc,"f1_score":f1_score,"precision":precision,"recall":recall}
         return {"accuracy":acc,"f1_score":f1_score}
+    
+    if args.model_name == 'diffuser':
+        tokenizer = RobertaTokenizer(tokenizer_file = f"{repo_dir}/roberta-tokenizer/tokenizer.json",
+                                     vocab_file     = f"{repo_dir}/roberta-tokenizer/vocab.json",
+                                     merges_file    = f"{repo_dir}/roberta-tokenizer/merges.txt",
+                                     max_length     = max_length)
+    elif args.model_name == 'automodel':
+        from transformers import AutoTokenizer
+        tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
-    # create cache for dataset
-    dataset_dir = join(droot, "DATASETS")
-    if not os.path.isdir(dataset_dir): os.makedirs(dataset_dir)
-    dataset = load_dataset(args.dataset_name, cache_dir=dataset_dir)
-    tokenizer = RobertaTokenizer(tokenizer_file = f"{repo_dir}/roberta-tokenizer/tokenizer.json",
-                                 vocab_file     = f"{repo_dir}/roberta-tokenizer/vocab.json",
-                                 merges_file    = f"{repo_dir}/roberta-tokenizer/merges.txt",
-                                 max_length     = max_length)
     # save tokenized dataset
-    dataset_dir = join(droot, "DATASETS", f"tokenized_{args.dataset_name}")
-    if not os.path.isdir(dataset_dir):         
+    tokenized_dataset_dir = join(droot, "DATASETS", f"tokenized_{args.dataset_name}")
+    if not os.path.isdir(tokenized_dataset_dir):         
         print("Downloading data!") if global_rank == 0 or not train_with_ddp else None
+        # create cache for dataset
+        dataset = get_dataset(args.dataset_name, join(droot, "DATASETS"))
+
         tokenized_dataset = dataset.map(preprocess_function, batched=True)
-        tokenized_dataset = tokenized_dataset.map(remove_columns=["text"])
-        os.makedirs(dataset_dir)
-        tokenized_dataset.save_to_disk(dataset_dir)
+        column_names = get_dataset_cols(tokenized_dataset)
+        if 'text' in column_names:
+            tokenized_dataset = tokenized_dataset.map(remove_columns=['text'])
+        if not isdir(tokenized_dataset_dir): os.makedirs(tokenized_dataset_dir)
+        tokenized_dataset.save_to_disk(tokenized_dataset_dir)
+        del dataset  # alleviate memory
     else:        
-        print("Data downloaded, loading from local now!") if global_rank == 0 or not train_with_ddp  else None
-        tokenized_dataset = load_from_disk(dataset_dir)
+        print("Data downloaded, loading from local now! \n") if global_rank == 0 or not train_with_ddp else None
+        tokenized_dataset = load_from_disk(tokenized_dataset_dir)
+    if args.dataset_name != 'imdb':
+        tokenized_dataset = process_dataset_cols(tokenized_dataset)
+
+    keys = list(tokenized_dataset.keys())
+    if len(keys) == 1:
+        tokenized_dataset = tokenized_dataset[keys[0]].train_test_split(0.5)
+    train_dataset = tokenized_dataset["train"]
+    eval_dataset = tokenized_dataset["test"]
+    #del tokenized_dataset  # alleviate memory         
 
     # ---------- REMOVE LATER ----------  
-    divider = 250  
-    tokenized_dataset['test'] = tokenized_dataset['test'].filter(lambda example, idx: idx % divider == 0, with_indices=True)
-
+    divider = args.divider  # downsize eval_dataset
+    eval_dataset = eval_dataset.filter(lambda example, idx: idx % divider == 0, with_indices=True)
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
     #config = DiffuserConfig.from_json_file(f"{repo_dir}/models/config.json")
+
+    # ---------------------------------------- 2. Model setup ----------------------------------------
+
     config = DiffuserConfig.from_json_file(f"{repo_dir}/models/config_simple.json")
-    config.num_labels = 2
-    with_frac = args.with_frac
-    attn_setup = {"with_frac":with_frac}
+    config.num_labels = len(set(train_dataset['label']))    
+    attn_setup = {"with_frac": args.with_frac}
     if global_rank == 0 or not train_with_ddp:
         print("-"*25)
-        print(f"with_frac = {with_frac} and gamma = {args.gamma}")
-        print("-"*25 + "\n")
-    if with_frac:
-        attn_setup["gamma"] = args.gamma  
+        print(f"with_frac = {args.with_frac} and gamma = {args.gamma}")
+        print("-"*25 + "\n")  
     if args.model_name == 'diffuser':
-        model =  DiffuserForSequenceClassification(config, **attn_setup).to(dev)
-        use_dgl = not with_frac    
+        if args.with_frac:
+            attn_setup["gamma"] = args.gamma        
+        use_dgl = not args.with_frac
+        model =  DiffuserForSequenceClassification(config, **attn_setup).to(dev)        
+    elif args.model_name == 'automodel':
+        from transformers import AutoModelForSequenceClassification
+        model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased").to(dev)
     # can add other options here
 
     if args.model_dir == None:
@@ -170,18 +241,9 @@ if __name__ == '__main__':
                           }
     if args.max_steps != None:
         training_args_dict["max_steps"] = args.max_steps
-    training_args = TrainingArguments(**training_args_dict)
-    
-    if dev.type != "cpu":
-        device_name = "GPU"
-    else:
-        device_name = "CPU"
-    if not train_with_ddp:
-        device_total = 1
-    else:
-        device_total = world_size
+    training_args = TrainingArguments(**training_args_dict)    
             
-    steps_per_train_epoch = int(len(tokenized_dataset['train'])/(training_args.per_device_train_batch_size*device_total*training_args.gradient_accumulation_steps ))
+    steps_per_train_epoch = int(len(train_dataset)/(training_args.per_device_train_batch_size*device_total*training_args.gradient_accumulation_steps ))
     if global_rank == 0 or not train_with_ddp:
         print("-"*25)
         print(f"steps_per_train_epoch {steps_per_train_epoch}")
@@ -190,29 +252,33 @@ if __name__ == '__main__':
         print(f"gradient_accumulation_steps: {training_args.gradient_accumulation_steps}")
         print("-"*25 + "\n")
 
-    if isinstance(training_args.num_train_epochs, int) and args.max_steps == None:
+    if training_args.num_train_epochs >= 1 and args.max_steps == None:
         training_args.eval_steps    = int(steps_per_train_epoch)
-        training_args.logging_steps = int(steps_per_train_epoch/5)
+        #training_args.logging_steps = int(steps_per_train_epoch/5)
+        training_args.logging_steps = int(steps_per_train_epoch/3)
         training_args.save_steps    = int(steps_per_train_epoch)
 
-    trainer = graphTrainer(
-        use_dgl = use_dgl,
-        model = model,
-        config = config,
-        args = training_args,
-        train_dataset = tokenized_dataset["train"],
-        eval_dataset = tokenized_dataset["test"],
-        tokenizer = tokenizer,
-        data_collator = data_collator,
-        compute_metrics = compute_metrics,
-        preprocess_logits_for_metrics = preprocess_logits_for_metrics
-    )
+    trainer_kwargs = {'model': model,                      
+                      'args': training_args,
+                      'train_dataset': train_dataset,
+                      'eval_dataset': eval_dataset,
+                      'tokenizer': tokenizer,
+                      'data_collator': data_collator,
+                      'compute_metrics': compute_metrics,
+                      'preprocess_logits_for_metrics': preprocess_logits_for_metrics
+                      }
+    if args.model_name == 'diffuser':
+        trainer_kwargs['use_dgl'] = use_dgl
+        trainer_kwargs['config'] = config
+        trainer = graphTrainer(**trainer_kwargs)
+    else:
+        from transformers import Trainer
+        trainer = Trainer(**trainer_kwargs)
 
-    t0_train = time()  # record train time
+    t0_train = time()  # record train time    
     trainer.train(ignore_keys_for_eval=["loss", "hidden_states", "attentions", "global_attentions"])
     train_secs = time() - t0_train
 
-    sleep(5)
     model_dir = args.model_dir
     if not os.path.isdir(model_dir): os.makedirs(model_dir)
     # get performance history

d5e13e91b68552f6f8378e15b73d674f6bd94be3
CKQu1 <kevinqu1995@hotmail.com>
2023-11-02 11:17:47 +1100

    add project_ls and njoin() function

--- path_setup.py
+++ path_setup.py
@@ -1,5 +1,9 @@
 import os
-from os.path import join
+from os.path import join, normpath
 
+project_ls = ['ddl']
 droot = join(os.getcwd(), "droot")
 if not os.path.isdir(droot): os.makedirs(droot)
+
+def njoin(*args):
+    return normpath(join(*args))

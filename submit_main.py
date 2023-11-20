d6501572fdd5d4f1d4ff98c3bd7a3e160ddafa87
CKQu1 <kevinqu1995@hotmail.com>
2023-11-20 13:25:17 +1100

    debug create_model_dir()

--- submit_main.py
+++ submit_main.py
@@ -1,5 +1,6 @@
 import os
 from os.path import isfile, join
+from time import sleep
 from path_setup import droot
 repo_dir = os.getcwd()  # main dir
 
@@ -9,11 +10,17 @@ def get_instance(dir, s):
     dirnames = next(os.walk(dir))[1]
     if len(dirnames) > 0:
         for dirname in dirnames:        
-            if s in dirname and len(os.listdir(join(dir, dirname))) > 0:
-                try:                
-                    instances.append(int(dirname.split(s)[-1]))
-                except:
-                    pass        
+            # len(os.listdir(join(dir, dirname))) > 0
+            if s in dirname and "model=" in dirname:
+                #try:        
+                for s_part in dirname.split(s):
+                    if "model=" in s_part:
+                        start = s_part.find("model=") + 6
+                        end = s_part.find("_")
+                        instances.append(int(s_part[start:end]))
+                #except:
+                #    pass       
+        print(instances) 
         return max(instances) + 1 if len(instances)>0 else 0
     else:
         return 0
@@ -24,18 +31,18 @@ def create_model_dir(model_root_dir, **kwargs):
     dataset_name = kwargs.get('dataset_name', 'imdb')
     model_name = kwargs.get('model_name', 'diffuser')
     if (model_name == 'diffuser') and with_frac:                   
-        model_root_dir = join(model_root_dir, f"frac_{model_name}")
+        models_dir = join(model_root_dir, f"frac_{model_name}")
     else:            
-        model_root_dir = join(model_root_dir, f"{model_name}")
-    if not os.path.isdir(model_root_dir): os.makedirs(model_root_dir)    
+        models_dir = join(model_root_dir, f"{model_name}")
+    if not os.path.isdir(models_dir): os.makedirs(models_dir)    
     if with_frac:    
-        instance = get_instance(model_root_dir, f"gamma={gamma}")
-        model_dir = join(model_root_dir, f"model={instance}_gamma={gamma}")
+        instance = get_instance(models_dir, f"gamma={gamma}")
+        model_dir = join(models_dir, f"model={instance}_gamma={gamma}")
     else:
-        instance = get_instance(model_root_dir, "model=")
-        model_dir = join(model_root_dir, f"model={instance}")     
+        instance = get_instance(models_dir, "model=")
+        model_dir = join(models_dir, f"model={instance}")     
     if not os.path.isdir(model_dir): os.makedirs(model_dir)        
-    return model_dir    
+    return models_dir, model_dir    
 
 def command_setup(ngpus, ncpus, singularity_path, **kwargs):
     assert isfile(singularity_path), "singularity_path does not exist!"
@@ -121,12 +128,15 @@ def train_submit(script_name, ngpus, ncpus, kwargss, **kwargs):
                        #"walltime":'95:59:59', 
                        #"walltime":'71:59:59',
                        #"walltime":'59:59:59',
-                       "walltime":'47:59:59',
+                       "walltime":'35:59:59',
                        #"walltime":'39:59:59',
                        #"walltime":'35:59:59',
                        #"walltime":'29:59:59',
                        #"walltime":'23:59:59',
-                       "mem":"48GB"} 
+                       #"walltime":'23:59:59',
+                       "mem":"48GB"
+                       #"mem":"16GB"
+                       } 
         if len(additional_command) > 0:
             kwargs_qsub["additional_command"] = additional_command
 
@@ -136,25 +146,31 @@ def train_submit(script_name, ngpus, ncpus, kwargss, **kwargs):
 if __name__ == '__main__':
 
     # script for running
-    script_name = "main_seq_classification.py"
-    ngpus, ncpus = 0, 12
-    select = 1
-    train_with_ddp = True if max(ngpus, ncpus) > 1 else False    
+    script_name = "main_seq_classification.py" 
     #dataset_names = ['imdb']  # add or change datasets here
     dataset_names = ['rotten_tomatoes']
     
     debug_mode = False
     for dataset_name in dataset_names:
         if not debug_mode:
+            ngpus, ncpus = 0, 12
+            select = 1
+            train_with_ddp = True if max(ngpus, ncpus) > 1 else False   
+
             # empty dict is diffuser
             #kwargss = [{"with_frac":True, "gamma":0.2}, {"with_frac":True, "gamma":0.4},
             #           {"with_frac":True, "gamma":0.6}, {"with_frac":True, "gamma":0.8}]  
-            kwargss = [{"with_frac":True, "gamma":0.25}, 
-                       {"with_frac":True, "gamma":0.5}, {"with_frac":True, "gamma":0.75}]                         
+            #kwargss = [{"with_frac":True, "gamma":0.25}, 
+            #           {"with_frac":True, "gamma":0.5}, {"with_frac":True, "gamma":0.75}]  
+            kwargss = [{"with_frac":True, "gamma":0.5, "lr":3e-4}, {"with_frac":True, "gamma":0.5, "lr":3e-3}]    
+            #kwargss = [{"with_frac":True, "gamma":0.2}, {"with_frac":True, "gamma":0.8}]                              
             #kwargss = [{}, {"with_frac":True, "gamma":0.4}]                         
             #model_root_dir = join(droot, "renorm_tomatoes")  # W := exp(Q^T K)
             #model_root_dir = join(droot, "invrenorm_tomatoes")  # W := exp(-Q^T K)
-            model_root_dir = join(droot, "reg_tomatoes")  # regularized embedding
+            #model_root_dir = join(droot, "reg_tomatoes")  # regularized embedding
+            #model_root_dir = join(droot, "dereg_tomatoes")  # regularized embedding (detached frac lapl)
+            #model_root_dir = join(droot, "derhoandoutdegreg_tomatoes")  # regularized embedding (detached rho and out-degree)
+            model_root_dir = join(droot, "derhoreg_lrtest_tomatoes")  # regularized embedding (detached rho, droot/derhoreg_lrtest_tomatoes)
             common_kwargs = {"gradient_accumulation_steps":4,
                              "epochs":20,
                              "warmup_steps":50,
@@ -162,13 +178,16 @@ if __name__ == '__main__':
                              "per_device_train_batch_size":4,
                              "per_device_eval_batch_size":4}
         else:
-            kwargss = [{}, {"with_frac":True, "gamma":0.2}, {"with_frac":True, "gamma":0.4},
-                       {"with_frac":True, "gamma":0.6}, {"with_frac":True, "gamma":0.8}]  
-            model_root_dir = join(droot, "debug_mode9")
+            ngpus, ncpus = 0, 2
+            select = 1
+            train_with_ddp = True if max(ngpus, ncpus) > 1 else False   
+
+            kwargss = [{}, {"with_frac":True, "gamma":0.8, "lr":3e-4}]  
+            model_root_dir = join(droot, "debug_mode14")
             common_kwargs = {"gradient_accumulation_steps":2,
-                             "divider": 10,
-                             "max_steps": 20,
-                             "warmup_steps":0, "eval_steps":20, "logging_steps":20, "save_steps":20,
+                             "divider": 100,
+                             "max_steps": 200,
+                             "warmup_steps":0, "eval_steps":50, "logging_steps":50, "save_steps":50,
                              "per_device_train_batch_size":2,
                              "per_device_eval_batch_size":2} 
         if train_with_ddp:
@@ -178,7 +197,7 @@ if __name__ == '__main__':
         for idx in range(len(kwargss)):
             # function automatically creates dir
             kwargss[idx]["dataset_name"] = dataset_name
-            kwargss[idx]["model_dir"] = create_model_dir(model_root_dir, **kwargss[idx])            
+            models_dir, kwargss[idx]["model_dir"] = create_model_dir(model_root_dir, **kwargss[idx])         
 
         print(kwargss)        
         train_submit(script_name, ngpus, ncpus, kwargss,

dc26f6f67e537123ab3cf9921e25ef139b3f1985
CKQu1 <kevinqu1995@hotmail.com>
2024-02-29 18:04:36 +1100

    first commit, contains global constants

---
+++ constants.py
@@ -0,0 +1,11 @@
+from path_setup import njoin
+
+# path for saving all data
+DROOT = join(os.getcwd(), "droot")
+if not os.path.isdir(DROOT): os.makedirs(DROOT)
+
+# singularity path
+SPATH = "../built_containers/FaContainer_v2.sif"
+
+# project names
+PROJECTS = ["frac_attn"]

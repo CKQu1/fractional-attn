0571325b03202a5e4f9e759c01e224d970b1e28a
CKQu1 <kevinqu1995@hotmail.com>
2024-03-18 01:00:27 +1100

    debug and loop over batch_size and n_heads

--- nlp-tutorial/dijkstra.py
+++ nlp-tutorial/dijkstra.py
@@ -1,6 +1,5 @@
-import numpy as np
 import torch
-from time import time
+import torch.nn as nn
 
 """
 A priority-queue realization of the Dijkstra algorithm based on the following:
@@ -9,17 +8,27 @@ A priority-queue realization of the Dijkstra algorithm based on the following:
 """
 
 # Currently, this only works for single attention head
-class DijkstraPQ(object):
+class DijkstraPQ(nn.Module):
     """Priority Q implelmentation in PyTorch
 
     Args:
         object ([torch.Tensor]): [The Queue to work on]
     """
 
-    def __init__(self, val):
+    def __init__(self, reduction='none'):
+        super(DijkstraPQ, self).__init__()
+        self.reduction = reduction
+
+    # def __init__(self, val):
+    #     self.q = torch.tensor([[val, 0]])
+    #     # self.top = self.q[0]
+    #     # self.isEmpty = self.q.shape[0] == 0
+
+    def create_q(self, val):        
+
         self.q = torch.tensor([[val, 0]])
         # self.top = self.q[0]
-        # self.isEmpty = self.q.shape[0] == 0
+        # self.isEmpty = self.q.shape[0] == 0        
 
     def push(self, x):
         """Pushes x to q based on weightvalue in x. Maintains ascending order
@@ -31,13 +40,13 @@ class DijkstraPQ(object):
         Returns:
             [torch.Tensor]: [The queue tensor after correct insertion]
         """
-        if type(x) == np.ndarray:
-            x = torch.tensor(x)
+        #if type(x) == np.ndarray:
+        #    x = torch.tensor(x)
         if self.isEmpty():
             self.q = x
             self.q = torch.unsqueeze(self.q, dim=0)
             return
-        idx = torch.searchsorted(self.q.T[1], x[1])  # can be possibly leveraged to scale up to multiple attn heads
+        idx = torch.searchsorted(self.q.T[1].contiguous(), x[1].contiguous())  # can be possibly leveraged to scale up to multiple attn heads
         #print(idx)
         self.q = torch.vstack([self.q[0:idx], x, self.q[idx:]]).contiguous()
 
@@ -73,31 +82,81 @@ class DijkstraPQ(object):
         """
         return self.q.shape[0] == 0
 
-# Now dijkstra, with adjacency matrix(with graph weights as input)
+    def forward(self, adj):
 
-def dijkstra(adj):
-    n = adj.shape[0]
-    distance_matrix = torch.zeros([n, n])
-    for i in range(n):
-        u = torch.zeros(n, dtype=torch.bool)
-        d = np.inf * torch.ones(n)
-        d[i] = 0
-        q = DijkstraPQ(i)
-        while not q.isEmpty():
-            v, d_v = q.top()  # point and distance
-            v = v.int()
-            q.pop()
-            if d_v != d[v]:
-                continue
-            for j, py in enumerate(adj[v]):
-                if py == 0 and j != v:
-                    continue
-                else:
-                    to = j
-                    weight = py
-                    if d[v] + py < d[to]:
-                        d[to] = d[v] + py
-                        q.push(torch.Tensor([to, d[to]]))
-        distance_matrix[i] = d
+        #n = adj.shape[0]
+        n = adj.shape[-2]
+        #distance_matrix = torch.zeros([n, n])
+        distance_matrix = torch.zeros_like(adj)
+        for dim1 in range(adj.shape[0]):
+            for dim2 in range(adj.shape[1]):
+                for i in range(n):
+                    #u = torch.zeros(n, dtype=torch.bool)
+                    d = torch.inf * torch.ones(n)
+                    d[i] = 0
+                    #q = DijkstraPQ(i)
+                    self.create_q(i)
+                    #while not q.isEmpty():
+                    #print('q created!')  # delete
+                    #print(f'q = {self.q}')  # delete
+                    while not self.isEmpty():
+                        #v, d_v = q.top()  # point and distance
+                        #print(f'top = {self.top()}')  # delete                      
+                        v, d_v = self.top()
+                        #print(f'v = {v}')  # delete
+                        #print(f'd_v = {d_v}')  # delete
+                        v = v.int()
+                        #q.pop()
+                        self.pop()
+                        if d_v != d[v]:
+                            continue
+                        #for j, py in enumerate(adj[v]):
+                        for j, py in enumerate(adj[dim1,dim2,v]):
+                            if py == 0 and j != v:
+                                continue
+                            else:
+                                to = j
+                                weight = py
+                                if d[v] + py < d[to]:
+                                    d[to] = d[v] + py
+                                    #q.push(torch.Tensor([to, d[to]]))
+                                    self.push(torch.Tensor([to, d[to]]))
+                    #distance_matrix[i] = d        
+                    distance_matrix[dim1,dim2,i] = d
 
-    return distance_matrix  # added
+        return distance_matrix  # can return more, add here
+
+    # original version
+    # def forward(self, adj):
+
+    #     #n = adj.shape[0]
+    #     n = adj.shape[1]
+    #     distance_matrix = torch.zeros([n, n])
+    #     for i in range(n):
+    #         u = torch.zeros(n, dtype=torch.bool)
+    #         d = torch.inf * torch.ones(n)
+    #         d[i] = 0
+    #         #q = DijkstraPQ(i)
+    #         self.create_q(i)
+    #         #while not q.isEmpty():
+    #         while not self.isEmpty():
+    #             #v, d_v = q.top()  # point and distance
+    #             v, d_v = self.top()
+    #             v = v.int()
+    #             #q.pop()
+    #             self.pop()
+    #             if d_v != d[v]:
+    #                 continue
+    #             for j, py in enumerate(adj[v]):
+    #                 if py == 0 and j != v:
+    #                     continue
+    #                 else:
+    #                     to = j
+    #                     weight = py
+    #                     if d[v] + py < d[to]:
+    #                         d[to] = d[v] + py
+    #                         #q.push(torch.Tensor([to, d[to]]))
+    #                         self.push(torch.Tensor([to, d[to]]))
+    #         distance_matrix[i] = d        
+
+    #     return distance_matrix  # can return more, add here

767a8c5d25647e5dba5ec96157ed6afbe9bb5c1e
CKQu1 <kevinqu1995@hotmail.com>
2024-04-24 12:32:51 +1000

    make former names in NAMES_DICT shorter

--- nlp-classification/constants.py
+++ nlp-classification/constants.py
@@ -10,6 +10,6 @@ BPATH = njoin('/project')  # path for binding to singularity container
 SPATH = njoin('/project/frac_attn/built_containers/FaContainer_v3.sif')  # singularity container path
 
 MODEL_NAMES = ['fnsformer', 'v2fnsformer', 'v3fnsformer', 'dpformer', 'l2former']  # model str names
-NAMES_DICT = {'fnsformer': 'FNSFormer', 'v2fnsformer': 'FNSFormer', 'dpformer': 'Transformer',
+NAMES_DICT = {'fnsformer': 'FNS', 'v2fnsformer': 'FNS', 'v3fnsformer': 'FNS', 'dpformer': 'DP',
               'imdb': 'IMDb', 'rotten_tomatoes': 'Rotten Tomatoes',
               'eval_loss': 'Loss', 'eval_accuracy': 'Accuracy', 'eval_f1_score': r'$F_1$ score'}

f60c27ce48807c0a1242727eeee69de222176724
CKQu1 <kevinqu1995@hotmail.com>
2024-05-14 15:01:36 +1000

    use standalone for single core in command_setup_ddp()

--- vit-pytorch/qsub_parser.py
+++ vit-pytorch/qsub_parser.py
@@ -126,6 +126,9 @@ def command_setup_ddp(singularity_path, **kwargs):
     ncpus = kwargs.get('ncpus', 1) 
     ngpus = kwargs.get('ngpus', 0)
     select = kwargs.get('select', 1)
+
+    HOST_NODE_ADDR = kwargs.get('HOST_NODE_ADDR', 0)
+
     bind_path = kwargs.get('bind_path', BPATH)
     home_path = kwargs.get('home_path', os.getcwd())
     if len(singularity_path) > 0:
@@ -140,23 +143,43 @@ def command_setup_ddp(singularity_path, **kwargs):
         #command += f" CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node={ngpus}"
         #additional_command = 'run --backend=nccl'
         if select == 1:
-            command += f" torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:0"
-            command += f" --nnodes=1 --nproc_per_node={ngpus} --max-restarts=3"
-        else:
             # tolerates 3 failures
-            command += f" torchrun --nnodes={select} --nproc_per_node={ngpus}"
-            command += f" --max-restarts=3 --rdzv-id=$JOB_ID"
-            command += f" --rdzv-backend=c10d --rdzv-endpoint=$HOST_NODE_ADDR"
+            # command += f" torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:0"
+            # command += f" --nnodes=1 --nproc_per_node={ngpus} --max-restarts=3"
+
+            # command += f" torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:{HOST_NODE_ADDR}"
+            # command += f" --nnodes={select} --nproc_per_node={ngpus}"        
+
+            command += f" torchrun --standalone"
+            command += f" --nnodes={select} --nproc_per_node={ngpus}"                  
+                          
+        else:            
+            # command += f" torchrun --nnodes={select} --nproc_per_node={ngpus}"
+            # command += f" --max-restarts=3 --rdzv-id=$JOB_ID"
+            # command += f" --rdzv-backend=c10d --rdzv-endpoint=$HOST_NODE_ADDR"
+
+            command += f" torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:{HOST_NODE_ADDR}"
+            command += f" --nnodes={select} --nproc_per_node={ngpus}"            
+
     elif ngpus == 0 and ncpus > 1:
         #python -m torch.distributed.launch --nproc_per_node=4 --use_env train_classification_imdb.py run --backend=gloo
         #additional_command = 'run --backend=gloo'
         if select == 1:
-            command += f" torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:0"
-            command += f" --nnodes=1 --nproc_per_node={ncpus} --max-restarts=3"
+            # command += f" torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:0"
+            # command += f" --nnodes=1 --nproc_per_node={ncpus} --max-restarts=3"
+
+            # command += f" torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:{HOST_NODE_ADDR}"
+            # command += f" --nnodes={select} --nproc_per_node={ncpus}"                    
+
+            command += f" torchrun --standalone"
+            command += f" --nnodes={select} --nproc_per_node={ncpus}"                
         else:
-            command += f" torchrun --nnodes={select} --nproc_per_node={ncpus}"
-            command += f" --max-restarts=3 --rdzv-id=$JOB_ID"
-            command += f" --rdzv-backend=c10d --rdzv-endpoint=$HOST_NODE_ADDR"    
+            # command += f" torchrun --nnodes={select} --nproc_per_node={ncpus}"
+            # command += f" --max-restarts=3 --rdzv-id=$JOB_ID"
+            # command += f" --rdzv-backend=c10d --rdzv-endpoint=$HOST_NODE_ADDR"  
+              
+            command += f" torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:{HOST_NODE_ADDR}"
+            command += f" --nnodes={select} --nproc_per_node={ncpus}"                    
 
     if len(singularity_path) == 0:
         command = command[1:]

c0bf97533613524878a1f99aae30f3c3af7aabce
CKQu1 <kevinqu1995@hotmail.com>
2024-05-16 22:35:49 +1000

    debug get_batch(): replace trainloader with trainloader

--- vit-pytorch/ddp_main.py
+++ vit-pytorch/ddp_main.py
@@ -322,7 +322,7 @@ if __name__ == '__main__':
             data = testloader
         ix = torch.randint(len(data), (batch_size,))
         x = torch.stack([trainloader.dataset[i][0] for i in ix])
-        y = torch.tensor([trainloader.dataset[i][1] for i in ix])
+        y = torch.tensor([testloader.dataset[i][1] for i in ix])
         if device_type == 'cuda':
             # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
             x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)

0d8a074a469fad809a141a8e45df22d6179232ce
CKQu1 <kevinqu1995@hotmail.com>
2024-06-19 22:42:19 +1000

    add v2dmfns and v2opdmfns

--- vit-pytorch/constants.py
+++ vit-pytorch/constants.py
@@ -11,10 +11,12 @@ BPATH = njoin('/project')  # path for binding to singularity container
 SPATH = njoin('/project/frac_attn/built_containers/FaContainer_v3.sif')  # singularity container path
 
 MODEL_NAMES = ['fnsvit', 'opfnsvit', 'sinkvit', 'dpvit',
-               'dmfnsvit', 'opdmfnsvit']  # model str names
+               'dmfnsvit', 'opdmfnsvit',
+               'v2dmfnsvit', 'opdmfnsvit']  # model str names
                
 NAMES_DICT = {'fnsvit': 'FNS', 'opfnsvit': 'OPFNS',
               'dmfnsvit': 'DMFNS', 'opdmfnsvit': 'OPDMFNS',
+              'v2dmfnsvit': 'V2DMFNS', 'v2opdmfnsvit': 'V2OPDMFNS',
               'sinkvit': 'SINK', 'dpvit': 'DP', 
               'cifar10': 'CIFAR10',
               'val_loss': 'Eval Loss', 'val_acc': 'Eval Acc.', 'train_loss': 'Train Loss', 'train_acc': 'Train Acc.'

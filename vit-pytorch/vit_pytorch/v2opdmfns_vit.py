56a1caaf6e0c3504ad5338a986c602f5c95bb75c
CKQu1 <kevinqu1995@hotmail.com>
2024-06-24 00:35:46 +1000

    move neg sign for g_dist when alpha==2

--- vit-pytorch/vit_pytorch/v2opdmfns_vit.py
+++ vit-pytorch/vit_pytorch/v2opdmfns_vit.py
@@ -126,7 +126,7 @@ class OPDMFNSAttentionHead(nn.Module):
         if alpha < 2:
             attn_score = (1 + g_dist/bandwidth**0.5)**(-d_intrinsic-alpha)
         else:
-            attn_score = torch.exp((-g_dist/bandwidth**0.5)**(alpha/(alpha-1)))
+            attn_score = torch.exp(-(g_dist/bandwidth**0.5)**(alpha/(alpha-1)))
         attn_score_shape = attn_score.shape
 
         if a > 0:
@@ -267,7 +267,7 @@ class FasterOPDMFNSMultiHeadAttention(nn.Module):
         if alpha < 2:
             attn_score = (1 + g_dist/bandwidth**0.5)**(-d_intrinsic-alpha)
         else:
-            attn_score = torch.exp((-g_dist/bandwidth**0.5)**(alpha/(alpha-1)))
+            attn_score = torch.exp(-(g_dist/bandwidth**0.5)**(alpha/(alpha-1)))
         attn_score_shape = attn_score.shape
         if a > 0:
             K_tilde = torch.diag_embed(attn_score.sum(-1)**(-a)) @ attn_score @ torch.diag_embed(attn_score.sum(-2)**(-a))

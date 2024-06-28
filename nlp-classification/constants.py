1ba14327fc6b1d2be173ca7feb818978bc464b27
CKQu1 <kevinqu1995@hotmail.com>
2024-06-29 00:33:18 +1000

    replace V3 container with V4; use str names for models

--- nlp-classification/constants.py
+++ nlp-classification/constants.py
@@ -7,7 +7,7 @@ DROOT = njoin('/project/frac_attn/fractional-attn/nlp-classification', '.droot')
 FIGS_DIR = njoin(DROOT, 'figs_dir')
 
 BPATH = njoin('/project')  # path for binding to singularity container
-SPATH = njoin('/project/frac_attn/built_containers/FaContainer_v3.sif')  # singularity container path
+SPATH = njoin('/project/frac_attn/built_containers/FaContainer_v4.sif')  # singularity container path
 
 MODEL_NAMES = ['fnsformer', 
                'spfnsformer','spopfnsformer',
@@ -17,9 +17,17 @@ MODEL_NAMES = ['fnsformer',
                'l2former']  # model str names
                
 NAMES_DICT = {'fnsformer': 'FNS', 
-              'spfnsformer': r'FNS (\mathbb{S}^{{d-1}})', 'spopfnsformer': r'OPFNS (\mathbb{S}^{{d-1}})',               
-              'rdfnsformer': r'FNS (\mathbb{R}^{{d}})', 'rdopfnsformer': r'OPFNS (\mathbb{R}^{{d}})',
+              'spfnsformer': 'SPFNS', 'spopfnsformer': 'SPOPFNS',               
+              'rdfnsformer': 'RDFNS', 'rdopfnsformer': 'RDOPFNS',
               'sinkformer': 'SINK',
               'dpformer': 'DP',
               'imdb': 'IMDb', 'rotten_tomatoes': 'Rotten Tomatoes',
               'eval_loss': 'Loss', 'eval_accuracy': 'Accuracy', 'eval_f1_score': r'$F_1$ score'}
+
+# NAMES_DICT = {'fnsformer': 'FNS', 
+#               'spfnsformer': rf'FNS ($\mathbb{S}^{{d-1}}$)', 'spopfnsformer': rf'OPFNS ($\mathbb{S}^{{d-1}}$)',               
+#               'rdfnsformer': rf'FNS ($\mathbb{R}^{{d}}$)', 'rdopfnsformer': rf'OPFNS ($\mathbb{R}^{{d}}$)',
+#               'sinkformer': 'SINK',
+#               'dpformer': 'DP',
+#               'imdb': 'IMDb', 'rotten_tomatoes': 'Rotten Tomatoes',
+#               'eval_loss': 'Loss', 'eval_accuracy': 'Accuracy', 'eval_f1_score': r'$F_1$ score'}              

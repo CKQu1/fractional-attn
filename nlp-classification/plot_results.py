7cfcd30259de6f2a11de40911e82c8b0a28c8d6c
CKQu1 <kevinqu1995@hotmail.com>
2024-07-04 00:34:52 +1000

    specify model_root_dirs in func plot_ensembles; beautify figures

--- nlp-classification/plot_results.py
+++ nlp-classification/plot_results.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcl
+import math
 import numpy as np
 import pandas as pd
 
 from ast import literal_eval
+from matplotlib.transforms import ScaledTranslation
 from os import makedirs
 from os.path import isdir, isfile
+from string import ascii_lowercase
 from time import time
 from tqdm import tqdm
 from constants import *
@@ -29,187 +32,217 @@ python -i plot_results.py plot_ensembles .droot/formers_trained/layers=2-heads=8
 PROMPT input:
 """
 
-def plot_ensembles(model_root_dir, metrics=['eval_loss', 'eval_accuracy'], 
+def plot_ensembles(model_root_dirs, metrics=['eval_accuracy'], 
                    legend=True, display=False):
     global df, df_setting, df_filtered, fig_file, axs
     global final_performance
     global model_dir, config_dict, final_metrics, ensemble_metrics, metric_plot   
     global model_dirs, subpath 
 
+    model_root_dirs = str2ls(model_root_dirs)
     metrics = str2ls(metrics)
     legend = str2bool(legend)
-    display = str2bool(display)
+    display = str2bool(display)    
+    assert len(metrics) == 1
 
-    model_root_dir = model_root_dir.replace('\\','')
-    dirnames = sorted([dirname for dirname in os.listdir(model_root_dir) if 'former' in dirname])
-
-    # prompt to reorder file names
-    for dirname_idx, dirname in enumerate(dirnames):
-        for subdir in os.listdir(njoin(model_root_dir, dirname)):
-            if isfile(njoin(model_root_dir, dirname, subdir, 'run_performance.csv')):
-                final_performance = pd.read_csv(njoin(model_root_dir, dirname, subdir, 'final_performance.csv'))
-                dataset = final_performance.loc[0,'dataset_name']
-                print(f'Index {dirname_idx}: {dirname}')
-                break        
-    
-    print(f'Dataset: {dataset}')
-    print(f'model_root_dir = {model_root_dir}')
-
-    dirname_idxs = input('Order of dirnames:')
-    dirname_idxs = [int(dirname_idx) for dirname_idx in dirname_idxs.split(',')]
-    assert len(dirname_idxs) <= len(dirnames), 'dirname_idxs cannot exceed dirnames'
-    dirnames = [dirnames[dirname_idx] for dirname_idx in dirname_idxs]
-    print(f'{metrics} \n')    
-
-    nrows, ncols = 1, len(metrics)
-    figsize = (2.5*ncols,2.5*nrows)
-    fig, axs = plt.subplots(nrows,ncols,figsize=figsize,
-                            sharex=True,sharey=False)
-    #if axs.ndim == 1:
+    if len(model_root_dirs) <= 3:
+        nrows = 1
+        ncols = len(model_root_dirs)
+    else:
+        nrows = 2
+        ncols = math.ceil(len(model_root_dirs)/nrows)
+    figsize = (3*ncols,3*nrows)
+    fig, axs = plt.subplots(nrows,ncols,figsize=figsize,sharex=True,sharey=True)
     if nrows == 1:
         if ncols > 1:
             axs = np.expand_dims(axs, axis=0)
         else:
-            axs = np.expand_dims(axs, axis=[0,1])            
+            axs = np.expand_dims(axs, axis=[0,1])     
+    axs = axs.flatten()
 
-    # get model config
-    qk_share = 'qkv' if 'qkv' in model_root_dir else 'qqv'
-    config_dict = {'qk_share': qk_share}
-    for ls in model_root_dir.split('/'):
-        for ele in ls.split('-'):
-            if '=' in ele:
-                key, val = ele.split('=')
-                config_dict[key] = val
+    for mr_ii, model_root_dir in enumerate(model_root_dirs):
 
-    idx = 0  # axs dim 0
-    model_names = []
-    model_types = {}
-    model_linestyles = {}
-    model_markers = {}
-    model_colors = {}
-    N_model_types = 0        
-    for jdx, dirname in enumerate(dirnames):             
-        ensemble_dir = njoin(model_root_dir,dirname)
-        model_dirs = []
-        for subdir in os.listdir(ensemble_dir):
-            subpath = njoin(ensemble_dir,subdir)
-            if 'model=' in subdir and isfile(njoin(subpath,'final_performance.csv')):
-                model_dirs.append(subpath.replace('\\',''))
+        model_root_dir = model_root_dir.replace('\\','')
+        dirnames = sorted([dirname for dirname in os.listdir(model_root_dir) if 'former' in dirname])
 
-        # get type of transformer
-        df_setting = pd.read_csv(njoin(model_dirs[0],'final_performance.csv'))
-        model_type = model_name = NAMES_DICT[dirname.split('-')[0]]
-        if model_type not in model_types.keys():                
-            model_types[model_name] = 1
-            N_model_types += 1
-        else:
-            model_types[model_name] += 1            
-        if model_name not in model_names:
-            model_names.append(model_name)
-        if 'fnsformer' in dirname:
-            #alpha, bandwidth  = df_setting.loc[0,['alpha','bandwidth']]      
-            bandwidth = df_setting.loc[0,'bandwidth']
-            if 'a' in df_setting.columns:
-                a = df_setting.loc[0,'a']
+        # prompt to reorder file names
+        for dirname_idx, dirname in enumerate(dirnames):
+            for subdir in os.listdir(njoin(model_root_dir, dirname)):
+                if isfile(njoin(model_root_dir, dirname, subdir, 'run_performance.csv')):
+                    final_performance = pd.read_csv(njoin(model_root_dir, dirname, subdir, 'final_performance.csv'))
+                    dataset = final_performance.loc[0,'dataset_name']
+                    print(f'Index {dirname_idx}: {dirname}')
+                    break        
+        
+        print(f'Dataset: {dataset}, model_root_dir = {model_root_dir} \n')
+
+        dirname_idxs = input('Order of dirnames:')
+        dirname_idxs = [int(dirname_idx) for dirname_idx in dirname_idxs.split(',')]
+        assert len(dirname_idxs) <= len(dirnames), 'dirname_idxs cannot exceed dirnames'
+        dirnames = [dirnames[dirname_idx] for dirname_idx in dirname_idxs]
+        print(f'{metrics} \n')            
+
+        # get model config
+        qk_share = 'qkv' if 'qkv' in model_root_dir else 'qqv'
+        config_dict = {'qk_share': qk_share}
+        for ls in model_root_dir.split('/'):
+            for ele in ls.split('-'):
+                if '=' in ele:
+                    key, val = ele.split('=')
+                    config_dict[key] = val
+
+        idx = 0  # axs dim 0
+        model_names = []
+        model_types = {}
+        model_linestyles = {}
+        model_markers = {}
+        N_model_types = 0        
+        for jdx, dirname in enumerate(dirnames):             
+            ensemble_dir = njoin(model_root_dir,dirname)
+            model_dirs = []
+            for subdir in os.listdir(ensemble_dir):
+                subpath = njoin(ensemble_dir,subdir)
+                if 'model=' in subdir and isfile(njoin(subpath,'final_performance.csv')):
+                    model_dirs.append(subpath.replace('\\',''))
+
+            # get type of transformer
+            df_setting = pd.read_csv(njoin(model_dirs[0],'final_performance.csv'))
+            model_type = model_name = NAMES_DICT[dirname.split('-')[0]]
+            if model_type not in model_types.keys():                
+                model_types[model_name] = 1
+                N_model_types += 1
             else:
-                a = 1
-            if 'alpha' in df_setting.columns:
-                alpha = df_setting.loc[0,'alpha']
-            else:
-                alpha = df_setting.loc[0,'beta']
-            #model_settings = rf'$\alpha$ = {alpha}, $\varepsilon$ = {bandwidth}'
-            model_settings = rf'$\alpha$ = {alpha}, $a$ = {a}'
-            # if alpha < 2:                        
-            #     d_intrinsic = df_setting.loc[0,'d_intrinsic']
-            #     model_settings += rf', $d$ = {d_intrinsic}'  #$d_{\mathcal{M}}$
-            model_name += f' ({model_settings})'
-        elif 'sinkformer' in dirname:
-            n_it  = df_setting.loc[0,'n_it']
-            model_settings = rf'iter = {n_it}'
-            model_name += f' ({model_settings})'
-        model_linestyles[model_name] = linestyles[model_types[model_type] - 1]
-        #model_linestyles[model_name] = ''
-        #model_markers[model_name] = markers[model_types[model_type] - 1]
-        model_colors[model_name] = colors[N_model_types - 1]
-
-        # ensemble of training instances for the same architecture
-        final_metrics = {}  # metrics of the final epoch
-        count = 0
-        ensemble_metrics = {}            
-        for model_dir in model_dirs:
-            df = pd.read_csv(njoin(model_dir, 'run_performance.csv'))     
-                                
-            for kdx, metric in enumerate(metrics):
-                df_filtered = df[df[metric].notna()]
-
-                if 'acc' in metric or 'f1' in metric:
-                    metric_plot = df_filtered.loc[:,metric] * 100
+                model_types[model_name] += 1            
+            if model_name not in model_names:
+                model_names.append(model_name)
+            if 'fnsformer' in dirname:
+                #alpha, bandwidth  = df_setting.loc[0,['alpha','bandwidth']]      
+                bandwidth = df_setting.loc[0,'bandwidth']
+                if 'a' in df_setting.columns:
+                    a = df_setting.loc[0,'a']
                 else:
-                    metric_plot = df_filtered.loc[:,metric]
-                if metric not in final_metrics.keys():
-                    final_metrics[metric] = [metric_plot.iloc[-1]]
+                    a = 1
+                if 'alpha' in df_setting.columns:
+                    alpha = df_setting.loc[0,'alpha']
                 else:
-                    final_metrics[metric].append(metric_plot.iloc[-1])
+                    alpha = df_setting.loc[0,'beta']
+                #model_settings = rf'$\alpha$ = {alpha}, $\varepsilon$ = {bandwidth}'
+                #model_settings = rf'$\alpha$ = {alpha}, $a$ = {a}'
+                model_settings = rf'$\alpha$ = {alpha}'
+                # if alpha < 2:                        
+                #     d_intrinsic = df_setting.loc[0,'d_intrinsic']
+                #     model_settings += rf', $d$ = {d_intrinsic}'  #$d_{\mathcal{M}}$
+                model_legend = f'{model_name} ({model_settings})'
+            elif 'sinkformer' in dirname:
+                n_it  = df_setting.loc[0,'n_it']
+                model_settings = rf'iter = {n_it}'
+                model_legend = f'{model_name} ({model_settings})'
+            model_linestyles[model_name] = linestyles[model_types[model_type] - 1]
+            #model_linestyles[model_name] = ''
+            #model_markers[model_name] = markers[model_types[model_type] - 1]            
 
-                if metric not in ensemble_metrics.keys():
-                    ensemble_metrics[metric] = [metric_plot]
-                else:
-                    ensemble_metrics[metric].append(metric_plot)
+            # ensemble of training instances for the same architecture
+            final_metrics = {}  # metrics of the final epoch
+            count = 0
+            ensemble_metrics = {}            
+            for model_dir in model_dirs:
+                df = pd.read_csv(njoin(model_dir, 'run_performance.csv'))     
+                                    
+                for kdx, metric in enumerate(metrics):
+                    df_filtered = df[df[metric].notna()]
 
-                epoch_eval_runtime = df[df['eval_runtime'].notna()].loc[:,'eval_runtime'].mean()
-                train_runtime, total_flos = df_setting.loc[0,['train_runtime', 'total_flos']]
-                for other_metric in ['epoch_eval_runtime', 'train_runtime', 'total_flos']:
-                    if other_metric not in final_metrics.keys():
-                        final_metrics[other_metric] = [locals()[other_metric]]
+                    if 'acc' in metric or 'f1' in metric:
+                        metric_plot = df_filtered.loc[:,metric] * 100
                     else:
-                        final_metrics[other_metric].append(locals()[other_metric])
-            
-            count += 1
-        final_metrics[f'count'] = count                        
-                    
-        print('-'*15)    
-        print(f'{model_name} on {dataset}')
-        for kdx, metric in enumerate(metrics):
+                        metric_plot = df_filtered.loc[:,metric]
+                    if metric not in final_metrics.keys():
+                        final_metrics[metric] = [metric_plot.iloc[-1]]
+                    else:
+                        final_metrics[metric].append(metric_plot.iloc[-1])
 
-            ensemble_metrics[metric] = pd.concat(ensemble_metrics[metric], axis=1).T
-            # quit()
+                    if metric not in ensemble_metrics.keys():
+                        ensemble_metrics[metric] = [metric_plot]
+                    else:
+                        ensemble_metrics[metric].append(metric_plot)
 
-            # ----- Plots -----
-            ensemble_mean = ensemble_metrics[metric].mean(0)
-            ensemble_std = ensemble_metrics[metric].std(0)
-            axs[idx,kdx].plot(df_filtered.loc[:,'epoch'], ensemble_mean,
-                                linestyle=model_linestyles[model_name], c=model_colors[model_name],
-                                #marker=model_markers[model_name], markersize=markersize,
-                                label=model_name) 
+                    epoch_eval_runtime = df[df['eval_runtime'].notna()].loc[:,'eval_runtime'].mean()
+                    train_runtime, total_flos = df_setting.loc[0,['train_runtime', 'total_flos']]
+                    for other_metric in ['epoch_eval_runtime', 'train_runtime', 'total_flos']:
+                        if other_metric not in final_metrics.keys():
+                            final_metrics[other_metric] = [locals()[other_metric]]
+                        else:
+                            final_metrics[other_metric].append(locals()[other_metric])
+                
+                count += 1
+            final_metrics[f'count'] = count                        
+                        
+            print('-'*15)    
+            print(f'{model_name} on {dataset}')
+            for kdx, metric in enumerate(metrics):
 
-            # std
-            # axs[idx,kdx].fill_between(df_filtered.loc[:,'epoch'], 
-            #                           ensemble_mean - ensemble_std, ensemble_mean + ensemble_std, 
-            #                           color=model_colors[model_name], alpha=0.5)                                          
+                ensemble_metrics[metric] = pd.concat(ensemble_metrics[metric], axis=1).T
+                # quit()                
 
-            if idx == 0:
-                axs[idx,kdx].set_title(NAMES_DICT[metric])
-            elif idx == nrows - 1:
-                axs[idx,kdx].set_xlabel('Epoch')
-            
-            if legend:
-                axs[0,0].legend(loc='upper left', #bbox_to_anchor=(0.5, 1.05),
-                                ncol=1, frameon=False)    
+                # ----- Plots -----
+                ensemble_mean = ensemble_metrics[metric].mean(0)
+                ensemble_std = ensemble_metrics[metric].std(0)
 
-            #axs[-1,kdx].set_ylabel(NAMES_DICT[dataset])
+                c_model = MODEL_CMAP(MODEL_CNORM(MODEL_NAMES.index(model_name.lower() + 'former')))
+                axs[mr_ii].plot(df_filtered.loc[:,'epoch'], ensemble_mean,
+                                    linestyle=model_linestyles[model_name], c=c_model,
+                                    #marker=model_markers[model_name], markersize=markersize,
+                                    label=model_legend) 
 
-            # ----- Messages -----
-            best = max(final_metrics[metric]) if 'acc' in metric or 'f1' in metric else min(final_metrics[metric])
-            worst = min(final_metrics[metric]) if 'acc' in metric or 'f1' in metric else max(final_metrics[metric])
-            median, mean = np.median(final_metrics[metric]), np.mean(final_metrics[metric])
-            print(f'{metric.upper()} best, median, mean, worst: {best}, {median}, {mean}, {worst}')
+                # std
+                # axs[mr_ii].fill_between(df_filtered.loc[:,'epoch'], 
+                #                         ensemble_mean - ensemble_std, ensemble_mean + ensemble_std, 
+                #                         color=c_model, alpha=0.5)                                          
 
-        #axs[idx,0].set_ylabel(NAMES_DICT[dataset])
-        print(f'Total ensembles: {ensemble_metrics[metric].shape[0]}')
-        print('\n')            
-        for other_metric in ['epoch_eval_runtime', 'train_runtime', 'total_flos']:
-            print(f'Average total {other_metric}: {np.mean(final_metrics[other_metric])}')
-        print('-'*15 + '\n')                    
+                # ----- Messages -----
+                best = max(final_metrics[metric]) if 'acc' in metric or 'f1' in metric else min(final_metrics[metric])
+                worst = min(final_metrics[metric]) if 'acc' in metric or 'f1' in metric else max(final_metrics[metric])
+                median, mean = np.median(final_metrics[metric]), np.mean(final_metrics[metric])
+                print(f'{metric.upper()} best, median, mean, worst: {best}, {median}, {mean}, {worst}')
+
+
+            #axs[idx,0].set_ylabel(NAMES_DICT[dataset])
+            print(f'Total ensembles: {ensemble_metrics[metric].shape[0]}')
+            print('\n')            
+            for other_metric in ['epoch_eval_runtime', 'train_runtime', 'total_flos']:
+                print(f'Average total {other_metric}: {np.mean(final_metrics[other_metric])}')
+            print('-'*15 + '\n')                    
+
+        # legend  
+        if mr_ii == 0:
+            #axs[mr_ii].legend(loc='best', ncol=2, frameon=False)           
+            axs[mr_ii].legend(bbox_to_anchor=(1, 1.3),
+                              loc='best', ncol=2, frameon=False)
+        # set titles
+        if mr_ii < nrows:            
+            title = r'$W_Q \neq W_K$' if qk_share == 'qkv' else r'$W_Q = W_K$'
+            axs[mr_ii].set_title(title)
+        # display bandwidth
+        if mr_ii % ncols == 1:            
+            axs[mr_ii].text(1.2, 0.5, rf'$\varepsilon = {{{bandwidth}}}$', transform=(
+                            axs[mr_ii].transAxes + ScaledTranslation(-20/72, +7/72, fig.dpi_scale_trans)),
+                            fontsize='medium', va='center', rotation='vertical')
+
+        # subplot labels
+        axs[mr_ii].text(
+            0.0, 1.0, f'({ascii_lowercase[mr_ii]})', transform=(
+                axs[mr_ii].transAxes + ScaledTranslation(-20/72, +7/72, fig.dpi_scale_trans)),
+            fontsize='medium', va='bottom', fontfamily='sans-serif')                               
+
+
+    # Add shared x and y labels
+    fig.text(0.5, 0.01, 'Epochs', ha='center')
+    fig.text(0, 0.5, 'Eval accuracy', va='center', rotation='vertical')    
+
+    # Add shared vertical label on the right
+    #fig.text(0.95, 0.5, 'Shared Right Y-Axis Label', ha='center', va='center', rotation=270)
+
+    # Adjust layout
+    #plt.tight_layout(rect=[0, 0, 0.93, 1])  # Leave space for the right label                 
 
     dataset_name_short = ''
     if '_' in dataset:
@@ -224,7 +257,7 @@ def plot_ensembles(model_root_dir, metrics=['eval_loss', 'eval_accuracy'],
         if not isdir(FIGS_DIR): makedirs(FIGS_DIR)
         if len(config_dict.keys()) != 0:
             layers, heads, hidden = int(config_dict['layers']), int(config_dict['heads']), int(config_dict['hidden'])
-            fig_file = f'prj={qk_share}-layers={layers}-heads={heads}-hidden={hidden}-'            
+            fig_file = f'layers={layers}-heads={heads}-hidden={hidden}-'            
             fig_file += '-'.join(model_names)+'_' + f'ds={dataset_name_short}'
         else:
             fig_file = '-'.join(model_names)+'_' + f'ds={dataset_name_short}'

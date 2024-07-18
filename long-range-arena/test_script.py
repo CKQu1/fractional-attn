b09e7033d0d14ef6b42a97fa9d2a8450e9cfd096
CKQu1 <kevinqu1995@hotmail.com>
2024-07-18 15:53:21 +1000

    include more breakdown of pathfinder dataset setup

--- long-range-arena/test_script.py
+++ long-range-arena/test_script.py
@@ -21,6 +21,7 @@
 # lengths = aux_data.get('lengths', None)
 
 # -------------------------------------------
+import torch
 from pathlib import Path
 cache_dir = DEFAULT_CACHE_DIR_ROOT = Path('./cache_dir/')
 
@@ -31,3 +32,24 @@ dir_name = f'./.raw_datasets/lra_release/lra_release/pathfinder{resolution}'
 
 dataset_obj = PathFinder(name, data_dir=dir_name, resolution=resolution)
 dataset_obj.cache_dir = Path(cache_dir) / name
+
+# ---- dataloader.lra -----
+
+from dataloaders.lra import PathFinderDataset
+
+# under setup()
+torch.multiprocessing.set_sharing_strategy("file_system")
+dataset = PathFinderDataset(dataset_obj.data_dir, transform=dataset_obj.default_transforms())
+len_dataset = len(dataset)
+val_len = int(dataset_obj.val_split * len_dataset)
+test_len = int(dataset_obj.test_split * len_dataset)
+train_len = len_dataset - val_len - test_len
+(
+    dataset_obj.dataset_train,
+    dataset_obj.dataset_val,
+    dataset_obj.dataset_test,
+) = torch.utils.data.random_split(
+    dataset,
+    [train_len, val_len, test_len],
+    generator=torch.Generator().manual_seed(dataset_obj.seed),
+)

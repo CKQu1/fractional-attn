06628e0bdabfb2ee40d78d1826f3076bcdd4bde8
CKQu1 <kevinqu1995@hotmail.com>
2024-07-20 19:10:50 +1000

    add physics cluster paths; clean up

--- long-range-arena/constants.py
+++ long-range-arena/constants.py
@@ -1,8 +1,16 @@
 import os
 from mutils import njoin
 
+# ----- ARTEMIS -----
 PROJECTS = ['phys_DL','PDLAI','dnn_maths','dyson','vortex_dl','frac_attn', 'ddl']
+# -------------------
 
+# ----- PHYSICS -----
+PHYSICS_SOURCE = '/usr/physics/python/Anaconda3-2022.10/etc/profile.d/conda.sh'
+PHYSICS_CONDA = 'frac_attn'
+# -------------------
+
+# ----- PATHS -----
 RT = os.path.abspath(os.getcwd())
 if 'project' in RT:
     DROOT = njoin('/project/frac_attn/fractional-attn/long-range-arena', '.droot')
@@ -14,9 +22,7 @@ FIGS_DIR = njoin(DROOT, 'figs_dir')
 BPATH = njoin('/project')  # path for binding to singularity container
 SPATH = njoin('/project/frac_attn/built_containers/FaContainer_v5.sif')  # singularity container path
 
-DATASET_NAMES = ['imdb-classification', 'lra-cifar-classification',
-                 'listops-classification', 'aan-classification',
-                 'pathfinder-classification', 'pathx-classification'] 
+# ----- TRAINING -----
 
 MODEL_NAMES = ['fnsformer', 'opfnsformer', 'sinkformer', 'dpformer']  # model str names
 NAMES_DICT = {'fnsformer': 'FNS', 'opfnsformer': 'OPFNS', 
@@ -26,3 +32,15 @@ NAMES_DICT = {'fnsformer': 'FNS', 'opfnsformer': 'OPFNS',
               'train_loss': 'Train loss', 'val_loss': 'Val loss', 'val_bleu': 'Bleu', 
               'eval_f1_score': r'$F_1$ score'
               }
+
+DATASET_NAMES = ['imdb-classification', 'lra-cifar-classification',
+                 'listops-classification', 'aan-classification',
+                 'pathfinder-classification', 'pathx-classification']     
+
+DATASET_EPOCHS = {'imdb-classification':5, 'lra-cifar-classification':100,
+                  'listops-classification':5, 'aan-classification':5,
+                  'pathfinder-classification':5, 'pathx-classification':5}                
+
+DATASET_STEPS = {'imdb-classification':100000, 'lra-cifar-classification':100000,
+                  'listops-classification':500000, 'aan-classification':500000,
+                  'pathfinder-classification':500000, 'pathx-classification':500000}                                

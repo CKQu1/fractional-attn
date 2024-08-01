369737f000e6d413dc3b975aba556b94a8653a6b
CKQu1 <kevinqu1995@hotmail.com>
2024-08-01 15:14:06 +1000

    add figure settings

--- vit-pytorch/constants.py
+++ vit-pytorch/constants.py
@@ -1,5 +1,7 @@
+import matplotlib as mpl
 import os
 from mutils import njoin
+from matplotlib.cm import get_cmap
 
 # ----- ARTEMIS -----
 PROJECTS = ['phys_DL','PDLAI','dnn_maths','dyson','vortex_dl','frac_attn', 'ddl']
@@ -33,3 +35,21 @@ NAMES_DICT = {'fnsvit': 'FNS', 'opfnsvit': 'OPFNS',
               'cifar10': 'CIFAR10',
               'val_loss': 'Eval Loss', 'val_acc': 'Eval Acc.', 'train_loss': 'Train Loss', 'train_acc': 'Train Acc.'
               }
+
+# color for model hyperparameters
+#HYP_CM = 'gist_ncar'
+HYP_CM = 'turbo'
+HYP_CMAP = get_cmap(HYP_CM)
+HYP_CNORM = mpl.colors.Normalize(vmin=1, vmax=2)
+
+def HYP_TRANS(alpha):
+    min_trans, max_trans = 0.5, 1
+    min_alpha, max_alpha = 1, 2
+    m = (min_trans - max_trans) / (max_alpha - min_alpha)
+    b = max_trans - m * min_alpha
+    return m*alpha + b
+
+LINESTYLE_DICT = {'spfnsvit': 'solid', 'spopfnsvit': (0,(5,1)),               
+                  'rdfnsvit': 'solid', 'rdopfnsvit': (0,(5,1)),
+                  'sinkvit': (0,(1,1)),
+                  'dpvit': (0,(5,5))}              

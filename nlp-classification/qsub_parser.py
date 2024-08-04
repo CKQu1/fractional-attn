7738023d520cb907e230528a366df460f9a508b8
CKQu1 <kevinqu1995@hotmail.com>
2024-08-05 00:45:16 +1000

    replace command_setup_ddp with version from vit-pytorch

--- nlp-classification/qsub_parser.py
+++ nlp-classification/qsub_parser.py
@@ -28,11 +28,11 @@ def qsub(command, pbs_array_data, **kwargs):
     # Create output folder.
     if not isdir(njoin(path,"job")): os.makedirs(njoin(path,"job"))
     # source virtualenv
-    # if 'source' in kwargs:
-    #     assert os.path.isfile(kwargs.get('source')), "source for virtualenv incorrect"
-    #     source_exists = 'true'
-    # else:
-    #     source_exists = 'false'
+    if 'source' in kwargs:
+        #assert os.path.isfile(kwargs.get('source')), "source for virtualenv incorrect"
+        source_exists = 'true'
+    else:
+        source_exists = 'false'
     # conda activate
     if 'conda' in kwargs:
         conda_exists = 'true'
@@ -131,8 +131,8 @@ conda activate frac_attn
 exit
 END"""  
 
-            #os.system(f'qsub {PBS_SCRIPT}')
-            print(PBS_SCRIPT)
+            os.system(f'qsub {PBS_SCRIPT}')
+            #print(PBS_SCRIPT)
 
 
 # N is the total number of projects

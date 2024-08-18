6557ca98c3dbc6b0a6eac59bbd9329e64b669269
CKQu1 <kevinqu1995@hotmail.com>
2024-08-18 23:32:49 +1000

    first commit, copied from nlp-classification

---
+++ LRA/models/attention_rdopfnsformer.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+import math
+
+from torch.nn import functional as F
+
+# import functorch
+# from torchmetrics.functional import pairwise_cosine_similarity
+# batched_pairwise_cosine_similarity = functorch.vmap(pairwise_cosine_similarity)
+
+class RDOPFNSAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.drop_attn = torch.nn.Dropout(p = config["attention_dropout"])
+        self.head_dim = config["head_dim"]
+        self.num_head = config["num_head"]
+
+        self.alpha = config["alpha"]
+        self.bandwidth = config["bandwidth"]     
+        self.a = config["a"]   
+
+        if self.alpha < 2:
+            self.d_intrinsic = config["d_intrinsic"]
+        self.mask_val = config["mask_val"]       
+
+    def forward(self, Q, K, V, mask):
+
+        alpha, bandwidth, a = self.alpha, self.bandwidth, self.a
+        d_intrinsic, mask_val = self.d_intrinsic, self.mask_val
+
+        # geodesic distance on R^d        
+        g_dist = torch.cdist(Q, K, p=2)  # (H,B,N,N)
+
+        # Calculate the attention scores
+        if alpha < 2:
+            attn_score = (1 + g_dist / bandwidth**0.5) ** (-d_intrinsic - alpha)
+        else:
+            attn_score = torch.exp((-g_dist / bandwidth**0.5) ** (alpha / (alpha - 1)))
+
+        if mask is not None:        
+            # type 1: key_pad_mask
+            mask_expanded = mask.unsqueeze(1).unsqueeze(2).expand([-1,self.num_head,1,-1])
+            # print(f'mask_expanded shape: {mask_expanded.shape}')
+            # print(mask_expanded)
+            # quit()
+            attn_score = attn_score.masked_fill(mask_expanded==0, mask_val)
+
+        if a == 0:
+            # attn_score = attn_score.masked_fill_(attention_mask==0, -1e9) # Mask
+            attn = F.normalize(
+                attn_score, p=1, dim=3
+            )  # can do this as the attn weights are always positive
+        else:
+            """
+            D_inv_row = torch.diag_embed(
+                attn_score.sum(-1) ** (-a)
+            )  # inverse of degree matrix of attn_score
+            D_inv_col = torch.diag_embed(
+                attn_score.sum(-2) ** (-a)
+            )  # inverse of degree matrix of attn_score
+            K_tilde = D_inv_row @ attn_score @ D_inv_col
+            # K_tilde = K_tilde.masked_fill_(attention_mask==0, -1e9) # Mask
+            """
+            N_R = attn_score.sum(-1)  # row sum
+            N_C = attn_score.sum(-2)  # col sum                
+            K_tilde = (N_R**(-a)).unsqueeze(-1) * attn_score * (N_C**(-a)).unsqueeze(-2) 
+
+            attn = F.normalize(
+                K_tilde, p=1, dim=3
+            )  # can do this as the attn weights are always positive
+
+        attn = self.drop_attn(attn)
+        #X = torch.matmul(attn, V)
+        X = attn @ V
+
+        return X
+
+    def extra_repr(self):
+        return f'spopfnsformer-alpha={self.alpha}-bandwidth={self.bandwidth}-a={self.a}'

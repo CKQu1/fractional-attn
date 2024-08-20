475fe62f870d7c9aeb39e344a54f58c52c5ca000
CKQu1 <kevinqu1995@hotmail.com>
2024-08-20 23:42:59 +1000

    update MEM_DICT

--- LRA/submit_main.py
+++ LRA/submit_main.py
@@ -16,17 +16,16 @@ if __name__ == '__main__':
     nstack = 1  
 
     # settings
-    TASKS = ["lra-listops", "lra-retrieval", "lra-text",  
-             "lra-image", "lra-pathfinder32-curv_contour_length_14"]
-    # TASKS = ["lra-retrieval", "lra-text", "lra-image"]             
-    MEM_DICT = {"lra-listops": "24GB", "lra-retrieval": "48GB", "lra-text": "48GB",  
-                "lra-image": "48GB", "lra-pathfinder32-curv_contour_length_14": "24GB"}
+    # TASKS = ["lra-listops", "lra-retrieval", "lra-text",  
+    #          "lra-image", "lra-pathfinder32-curv_contour_length_14"]
+    TASKS = ["lra-listops", "lra-pathfinder32-curv_contour_length_14"]
+    #TASKS = ["lra-retrieval", "lra-text", "lra-image"]             
+    MEM_DICT = {"lra-listops": "12GB", "lra-retrieval": "32GB", "lra-text": "32GB",  
+                "lra-image": "32GB", "lra-pathfinder32-curv_contour_length_14": "12GB"}
     seeds = [0]
 
-    #ROOT = njoin(DROOT, 'trained-models')
-    ROOT = njoin(DROOT, 'full-models-v2')
-    #ROOT = njoin(DROOT, 'cpu-run')
-    #ROOT = njoin(DROOT, 'gpu-run')
+    #ROOT = njoin(DROOT, 'V100-run')
+    ROOT = njoin(DROOT, 'V100-run-constantlr')
     job_path = njoin(ROOT, 'jobs_all')
 
     kwargss_all = []    
@@ -34,10 +33,10 @@ if __name__ == '__main__':
         for task in TASKS:
             
             # CPUs
-            select = 1; ngpus, ncpus = 0, 1; # mem = '24GB' 
+            #select = 1; ngpus, ncpus = 0, 1; # mem = '24GB' 
             # GPUs
             #select = 1; ngpus, ncpus = 1, 0; mem = '32GB'  # 6L8H (512 len)                                     
-            #select = 1; ngpus, ncpus = 1, 1; # mem = '24GB'  #2L2H
+            select = 1; ngpus, ncpus = 1, 1; # mem = '24GB'  #2L2H
             mem = MEM_DICT[task]
                             
             walltime = '23:59:59'                    
@@ -46,20 +45,20 @@ if __name__ == '__main__':
             #for qk_share in [True, False]:
 
             kwargss = []
-            for alpha in [1.2]:
-            #for alpha in [1.2, 2]:        
-            #for alpha in [1, 1.4, 1.8]:                    
-                #for bandwidth in [0.01, 0.1, 0.5, 1]:                                    
-                #for bandwidth in [0.1,1]:
+            for alpha in [1.2,2]:        
+            #for alpha in [1, 1.4, 1.8]:                                                       
                 for bandwidth in [1]:
                     kwargss.append({'attn':'opfns','alpha':alpha,'a': 0,'bandwidth':bandwidth,'manifold':'sphere'})
                     #kwargss.append({'attn':'opfns','alpha':alpha,'a': 0,'bandwidth':bandwidth,'manifold':'rd'})
 
-            # kwargss.append({'attn':'softmax'})
+            #if task == 'lra-text':
+            kwargss.append({'attn':'softmax'})
+            
             # for n_it in [3]:
             #     kwargss.append({'attn':'sink','n_it':n_it})
 
-            common_kwargs = {           
+            common_kwargs = {      
+               "lr_scheduler":    'constantlr',      
                 "task":            task,                         
                 "random":          seed,
                 "qkv_bias":        False

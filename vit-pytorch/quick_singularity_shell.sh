9628ede2141a34aca0383e51519dc16e6693d1d3
CKQu1 <kevinqu1995@hotmail.com>
2024-11-17 16:52:57 +1100

    first commit

--- vit-pytorch/quick_singularity_shell.sh
+++ vit-pytorch/quick_singularity_shell.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 PBS_O_WORKDIR="/project/frac_attn/fractional-attn" 
-cpath="../../built_containers/FaContainer_v5.sif" 
+#cpath="../../built_containers/FaContainer_v5.sif" 
+cpath="../../built_containers/pydl.img"
 bpath1="/project"
-bpath2="/etc/localtime"
-#singularity shell -B ${bpath1} --home ${PBS_O_WORKDIR} ${cpath}
-singularity shell -B ${bpath1} ${cpath}
+bpath2="/usr/bin/nvidia-smi"
+#singularity shell -B ${bpath} --home ${PBS_O_WORKDIR} ${cpath}
+#singularity shell --bind ${bpath1},${bpath2} ${cpath}
+singularity shell --nv --bind ${bpath1} ${cpath}

fa7486e2afebf568e5b6448d08e38f0db6402372
CKQu1 <kevinqu1995@hotmail.com>
2024-11-25 14:00:09 +1100

    intakes str args separated by ; to run main.py

---
+++ vit-pytorch/batch_main.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+
+#from main import train_vit
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='vit-pytorch/main.py training arguments')   
+    # training settings 
+    #parser.add_argument('--train_with_ddp', default=True, type=bool, help='to use DDP or not')
+    parser.add_argument('--arg_strss', type=str)
+
+    args = parser.parse_args()    
+
+    arg_strss = args.arg_strss.split(';')
+    for arg_strs in arg_strss:
+        #arg_strs = '--' + 'arg_strs.replace(',',' ')        
+        arg_strs = '--' + ' --'.join(arg_strs.split(','))
+
+        os.system(f'python main.py {arg_strs}')        
+
+    

0eec8190ab52979a2a3a17eb43ade6df99003a94
Cheng Kevin Qu <chqu7424@headnode.physics.usyd.edu.au>
2025-08-04 13:46:18 +1000

    change PHYSICS_CONDA,CLUSTER

--- vit-pytorch/constants.py
+++ vit-pytorch/constants.py
@@ -9,7 +9,7 @@ DROOT = njoin(RT, '.droot')
 FIGS_DIR = njoin(DROOT, 'figs_dir')
 SCRIPT_DIR = njoin(DROOT, 'submitted_scripts')
 
-CLUSTER = 'ARTEMIS' if 'project' in DROOT else 'PHYSICS' if 'headnode' in DROOT else 'FUDAN_BRAIN'
+CLUSTER = 'ARTEMIS' if 'project' in DROOT else 'PHYSICS' if 'taiji1' in DROOT else 'FUDAN_BRAIN'
 # -------------------
 
 # ----- ARTEMIS -----
@@ -21,7 +21,8 @@ SPATH = njoin('/project/frac_attn/built_containers/pydl.img')
 
 # ----- PHYSICS -----
 PHYSICS_SOURCE = '/usr/physics/python/Anaconda3-2022.10/etc/profile.d/conda.sh'
-PHYSICS_CONDA = 'frac_attn' if 'chqu7424' in RT else '~/conda'
+#PHYSICS_CONDA = 'frac_attn' if 'chqu7424' in RT else '~/conda'
+PHYSICS_CONDA = '/taiji1/chqu7424/myenvs/pydl' if 'chqu7424' in RT else '~/conda'
 # -------------------
 
 # ----- FUDAN-BRAIN -----
@@ -30,7 +31,7 @@ FUDAN_CONDA = 'frac_attn'
 
 # ----- MODELS -----
 
-MODEL_PREFIXES = ['fns', 'opfns', 'spfns', 'spopfns', 'rdfns', 'rdopfns', 'sink', 'opsink', 'dp', 'opdp']
+MODEL_PREFIXES = ['fns', 'opfns', 'spfns', 'spopfns', 'rdfns', 'oprdfns', 'sink', 'opsink', 'dp', 'opdp']
 MODEL_SUFFIX = 'vit'
 
 MODEL_NAMES = []
@@ -46,7 +47,7 @@ for MODEL_PREFIX in MODEL_PREFIXES:
     else:
         NAMES_DICT[MODEL_PREFIX + MODEL_SUFFIX] = MODEL_PREFIX.upper()
 
-NAMES_DICT.update({'cifar10': 'CIFAR10', 
+NAMES_DICT.update({'cifar10': 'CIFAR10', 'mnist': 'MNIST',
                    'val_loss': 'Test Loss', 'val_acc': 'Test Acc.', 
                    'train_loss': 'Train Loss', 'train_acc': 'Train Acc.'}
                    )               
@@ -65,7 +66,7 @@ def HYP_TRANS(alpha):
     return m*alpha + b
 
 LINESTYLE_DICT = {'spfns'+MODEL_SUFFIX: 'solid', 'spopfns'+MODEL_SUFFIX: 'solid',  # (0,(5,1))               
-                  'rdfns'+MODEL_SUFFIX: 'solid', 'rdopfns'+MODEL_SUFFIX: 'solid',
+                  'rdfns'+MODEL_SUFFIX: 'solid', 'oprdfns'+MODEL_SUFFIX: 'solid',
                   'sink'+MODEL_SUFFIX: (0,(5,5)), 'opsink'+MODEL_SUFFIX: (0,(5,5)),
                   'dp'+MODEL_SUFFIX: (0,(1,1)), 'opdp'+MODEL_SUFFIX: (0,(1,1))
                   }        

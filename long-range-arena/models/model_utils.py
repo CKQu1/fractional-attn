9700d8ca8caf077272b7b6f2e585925f97edc703
Cheng Kevin Qu <chqu7424@headnode.physics.usyd.edu.au>
2025-08-12 00:31:08 +1000

    first commit

---
+++ long-range-arena/models/model_utils.py
@@ -0,0 +1,25 @@
+import math
+import torch
+from torch import nn
+
+# https://github.com/tintn/vision-transformer-from-scratch/blob/main/vit.py
+class NewGELUActivation(nn.Module):
+    """
+    Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT). Also see
+    the Gaussian Error Linear Units paper: https://arxiv.org/abs/1606.08415
+
+    Taken from https://github.com/huggingface/transformers/blob/main/src/transformers/activations.py
+    """
+
+    def forward(self, input):
+        return (
+            0.5
+            * input
+            * (
+                1.0
+                + torch.tanh(
+                    math.sqrt(2.0 / math.pi)
+                    * (input + 0.044715 * torch.pow(input, 3.0))
+                )
+            )
+        )

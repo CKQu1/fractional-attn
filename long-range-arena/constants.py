3a9247285229c031941d92c71ac39de1143a8310
Cheng Kevin Qu <chqu7424@headnode.physics.usyd.edu.au>
2025-08-12 00:28:13 +1000

    copied vit-pytorch version

--- long-range-arena/constants.py
+++ long-range-arena/constants.py
@@ -1,38 +1,53 @@
+import matplotlib as mpl
 import os
 from mutils import njoin
+from matplotlib.cm import get_cmap
+
+# ----- GENERAL -----
+RT = os.path.abspath(os.getcwd())
+DROOT = njoin(RT, '.droot')
+FIGS_DIR = njoin(DROOT, 'figs_dir')
+SCRIPT_DIR = njoin(DROOT, 'submitted_scripts')
+
+CLUSTER = 'ARTEMIS' if 'project' in DROOT else 'PHYSICS' if 'taiji1' in DROOT else 'FUDAN_BRAIN'
+# -------------------
 
 # ----- ARTEMIS -----
 PROJECTS = ['phys_DL','PDLAI','dnn_maths','dyson','vortex_dl','frac_attn', 'ddl']
+BPATH = njoin('/project')  # path for binding to singularity container
+#SPATH = njoin('/project/frac_attn/built_containers/FaContainer_v5.sif')  # singularity container path
+SPATH = njoin('/project/frac_attn/built_containers/pydl.img')
 # -------------------
 
 # ----- PHYSICS -----
 PHYSICS_SOURCE = '/usr/physics/python/Anaconda3-2022.10/etc/profile.d/conda.sh'
-PHYSICS_CONDA = 'frac_attn'
+#PHYSICS_CONDA = 'frac_attn' if 'chqu7424' in RT else '~/conda'
+PHYSICS_CONDA = '/taiji1/chqu7424/myenvs/pydl' if 'chqu7424' in RT else '~/conda'
 # -------------------
 
-# ----- PATHS -----
-RT = os.path.abspath(os.getcwd())
-if 'project' in RT:
-    DROOT = njoin('/project/frac_attn/fractional-attn/long-range-arena', '.droot')
-else:
-    DROOT = njoin(RT, '.droot')
+# ----- FUDAN-BRAIN -----
+FUDAN_CONDA = 'frac_attn'
+# -------------------
 
-FIGS_DIR = njoin(DROOT, 'figs_dir')
+# ----- MODELS -----
 
-BPATH = njoin('/project')  # path for binding to singularity container
-SPATH = njoin('/project/frac_attn/built_containers/FaContainer_v5.sif')  # singularity container path
+MODEL_PREFIXES = ['fns', 'opfns', 'spfns', 'spopfns', 'rdfns', 'oprdfns', 'sink', 'opsink', 'dp', 'opdp']
+MODEL_SUFFIX = 'former'
+
+MODEL_NAMES = []
+NAMES_DICT = {}
+for MODEL_PREFIX in MODEL_PREFIXES:
+    MODEL_NAMES.append(MODEL_PREFIX + MODEL_SUFFIX)
+
+    if 'fns' in MODEL_PREFIX.lower():
+        if 'rd' in MODEL_PREFIX.lower():
+            NAMES_DICT[MODEL_PREFIX + MODEL_SUFFIX] = r'FRAC $(\mathbb{{R}}^d)$'
+        else:                    
+            NAMES_DICT[MODEL_PREFIX + MODEL_SUFFIX] = r'FRAC $(\mathcal{{S}}^{{d-1}})$'
+    else:
+        NAMES_DICT[MODEL_PREFIX + MODEL_SUFFIX] = MODEL_PREFIX.upper()
 
 # ----- TRAINING -----
-
-MODEL_NAMES = ['spfnsformer', 'spopfnsformer', 'sinkformer', 'dpformer']  # model str names
-NAMES_DICT = {'spfnsformer': 'SPFNS', 'spopfnsformer': 'SPOPFNS', 
-              'rdfnsformer': 'RDFNS', 'rdopfnsformer': 'RDOPFNS',
-              'sinkformer': 'SINK',
-              'dpformer': 'DP',
-              'iwslt14': 'IWSLT14',
-              'train_loss': 'Train loss', 'val_loss': 'Val loss', 'val_bleu': 'Bleu', 
-              'eval_f1_score': r'$F_1$ score'
-              }
 
 DATASET_NAMES = ['imdb-classification', 'lra-cifar-classification',
                  'listops-classification', 'aan-classification',

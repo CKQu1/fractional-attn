bf8057794dac9b9f0188257dfe4bf8c1dc96c619
Cheng Kevin Qu <chqu7424@headnode.physics.usyd.edu.au>
2025-08-20 00:55:54 +1000

    add constants from nlp-tutorial

--- long-range-arena/constants.py
+++ long-range-arena/constants.py
@@ -31,7 +31,7 @@ FUDAN_CONDA = 'frac_attn'
 
 # ----- MODELS -----
 
-MODEL_PREFIXES = ['fns', 'opfns', 'spfns', 'spopfns', 'rdfns', 'oprdfns', 'sink', 'opsink', 'dp', 'opdp']
+MODEL_PREFIXES = ['spfns', 'opspfns', 'rdfns', 'oprdfns', 'sink', 'opsink', 'dp', 'opdp']
 MODEL_SUFFIX = 'former'
 
 MODEL_NAMES = []
@@ -46,6 +46,46 @@ for MODEL_PREFIX in MODEL_PREFIXES:
             NAMES_DICT[MODEL_PREFIX + MODEL_SUFFIX] = r'FRAC $(\mathcal{{S}}^{{d-1}})$'
     else:
         NAMES_DICT[MODEL_PREFIX + MODEL_SUFFIX] = MODEL_PREFIX.upper()
+
+NAMES_DICT.update({'pathfinder-classification': 'PF',
+                   'train_loss': 'Train Loss', 'train_acc': 'Train Acc.',
+                   'val_loss': 'Test Loss', 'val_acc': 'Test Acc.', 
+                   'train_loss': 'Train Loss', 'train_acc': 'Train Acc.'}
+                   )
+
+# ----- COLORS -----
+
+# color for model hyperparameters
+#HYP_CM = 'gist_ncar'
+HYP_CM = 'turbo'
+HYP_CMAP = get_cmap(HYP_CM)
+HYP_CNORM = mpl.colors.Normalize(vmin=1, vmax=2)
+
+def HYP_TRANS(alpha):
+    min_trans, max_trans = 0.5, 1
+    min_alpha, max_alpha = 1, 2
+    m = (min_trans - max_trans) / (max_alpha - min_alpha)
+    b = max_trans - m * min_alpha
+    return m*alpha + b
+
+OTHER_COLORS = ['m', 'dimgray']
+OTHER_COLORS_DICT = {'sink'+MODEL_SUFFIX: OTHER_COLORS[0], 'dp'+MODEL_SUFFIX: OTHER_COLORS[1],
+                     'opsink'+MODEL_SUFFIX: OTHER_COLORS[0], 'opdp'+MODEL_SUFFIX: OTHER_COLORS[1]}
+
+# ----- LINESTYLE_DICT -----
+
+LINESTYLE_DICT = {'spfns'+MODEL_SUFFIX: 'solid', 'spopfns'+MODEL_SUFFIX: 'solid',  # (0,(5,1))               
+                  'rdfns'+MODEL_SUFFIX: 'solid', 'rdopfns'+MODEL_SUFFIX: 'solid',
+                  'opspfns'+MODEL_SUFFIX: 'solid', 'oprdfns'+MODEL_SUFFIX: 'solid', 
+                  'v2_rdfns'+MODEL_SUFFIX: 'solid', 'opv2_rdfns'+MODEL_SUFFIX: 'solid',
+                  #'sink'+MODEL_SUFFIX: (0,(5,5)),
+                  'sink'+MODEL_SUFFIX: (0,(5,1)),
+                  'opsink'+MODEL_SUFFIX: (0,(5,1)),
+                  'dp'+MODEL_SUFFIX: (0,(1,1)),
+                  'opdp'+MODEL_SUFFIX: (0,(1,1))
+                  }
+
+DEPTH_TO_MARKER = {1: '^', 2: 's', 3: 'p', 4: 'hexagon2'}
 
 # ----- TRAINING -----
 

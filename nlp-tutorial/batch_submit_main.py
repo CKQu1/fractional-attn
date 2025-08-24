15efb38d6198e4b7b729c759be640af15d1929f1
K. Qu <kevinqu1995@hotmail.com>
2025-08-24 12:46:19 +1000

    clean up code; pass in q

--- nlp-tutorial/batch_submit_main.py
+++ nlp-tutorial/batch_submit_main.py
@@ -1,12 +1,11 @@
 import os
 from datetime import datetime
+from itertools import product
 from os.path import isfile, isdir
 from time import sleep
 from constants import DROOT, CLUSTER
-from utils.mutils import njoin, get_seed, structural_model_root, str2bool
+from UTILS.mutils import njoin, get_seed, structural_model_root, str2bool
 from qsub_parser import job_setup, qsub, add_common_kwargs
-
-#from constants import DATASET_NAMES, MAX_LENS, MAX_LENS_DICT
 
 """
 torchrun --nnodes=1 --nproc_per_node=2 ddp_main.py --max_iters=5 --eval_interval=5\
@@ -14,207 +13,159 @@ torchrun --nnodes=1 --nproc_per_node=2 ddp_main.py --max_iters=5 --eval_interval
 """
 
 if __name__ == '__main__':
-      
-    DATASET_NAMES = ['imdb']  # 'imdb', 'glue-sst2', 'ag_news', 'emotion', 'yelp_polarity'
+          
     date_str = datetime.today().strftime('%Y-%m-%d')    
     script_name = "batch_main.py"
-    #nstack = 44
-    nstack = 2
+    nstack = 9
+    MEM_DICT = {1: '4GB', 2: '6GB', 3: '8GB', 4: '10GB', 5: '12GB', 6: '14GB'}
+    #CLUSTER = 'PHYSICS'  # can manually enter here too
+    q = 'taiji'  # 'l40s', 'taiji'        
 
-    #seeds = list(range(5))
-    #seeds = list(range(10))
-    seeds = [0]
-    
+    # ensembles
+    seeds = list(range(5))
+    # dataset
+    DATASET_NAMES = ['imdb']  # 'imdb', 'glue-sst2', 'ag_news', 'emotion', 'yelp_polarity'
+    # embeddings
+    fix_embed = False
+    if fix_embed:
+        pretrained_model_name = 'glove'  # glove, distilbert-base-uncased, albert-base-v2
+    # special masks
+    train_mask_type = None  # 'longformer', None
+    # architecture
+    is_ops = [True]
+    qk_shares = [True, False]
+    is_rescale_dist = True
+    is_resnet_scale = False
+    max_len = 512
     n_layers = 1
-    #if n_layers == 1:
     if n_layers < 4:
-        model_size = 'small'  
-        alphas = [1.2, 2]
-        #alphas = [1,1.2,1.4,1.6,1.8,2]        
-        bandwidths = [1]  # 'median'
-        manifolds = ['rd']  # ['rd', 'sphere', 'v2_rd']
+        hiddens = [8, 16 ,32 ,64]
+        #hiddens = [64]
+    else:                
+        hiddens = [256]     
+    # training
+    lr_scheduler_type = 'binary'  # 'constant'
+    if lr_scheduler_type == 'binary':
+        binary_ratio = 3/4  # for L-d-grid study
 
-        hidden = 8
-        fix_embed = False
-        is_ops = [True]
-        qk_shares = [True, False]
-        #qk_shares = [True]
-        is_rescale_dist = True
-        is_resnet_scale = False
+    # resources
+    is_use_gpu = False
+    select = 1
+    (ngpus,ncpus) = (1,1) if is_use_gpu else (0,1)                               
+    walltime = '23:59:59'
+    mem = MEM_DICT[n_layers]   
 
-        max_len = 512
-        #train_mask_type = 'longformer'
-        train_mask_type = None
+    # FNS
+    alphas = [1, 1.2, 1.4, 1.6, 1.8, 2]  # 1, 1.2, 1.4, 1.6, 1.8, 2
+    #alphas = [1]
+    bandwidths = [1]  # 'median'      
+    manifolds = ['rd']  # 'rd', 'v2_rd', 'sphere'       
+    # other types
+    is_train_others = True               
 
-        if fix_embed:
-            pretrained_model_name = 'glove'  # distilbert-base-uncased, albert-base-v2
+    kwargss_all = []    
+    for seed, dataset_name in product(seeds, DATASET_NAMES):                                                                                                         
+        for hidden, qk_share, is_op in product(hiddens, qk_shares, is_ops):
+            # setting up directories
+            f_prefix = f'{n_layers}L'
+            if is_resnet_scale:
+                f_prefix += '-RR'            
+            ROOT = njoin(DROOT, 'L-d-grid')
+            job_path = njoin(ROOT, 'jobs_all')
+            ROOT = njoin(ROOT, f'{f_prefix}-hidden={hidden}-max_len={max_len}')
+            if fix_embed:
+                ROOT += f'-{pretrained_model_name}'
+            if train_mask_type is not None:
+                ROOT += f'-mask={train_mask_type}'        
+            if is_rescale_dist:
+                ROOT += '-rescaled'                    
 
-        is_train_others = False
-    else:
-        model_size = 'large'
-        #alphas = [1,1.2,1.4,1.6,1.8,2]
-        alphas = [1.2, 2]
-        #bandwidths = [1]
-        bandwidths = [1]  # 'median'      
-        manifolds = ['rd']  # ['rd', 'v2_rd', 'sphere']
+            kwargss = []
+            # FNS
+            for alpha, bandwidth, manifold in product(alphas, bandwidths, manifolds):
+                kwargss.append({'model_name':'fnsformer','alpha':alpha,'a': 0,
+                                'bandwidth':bandwidth,'manifold':manifold})
+            # Other models
+            if is_train_others:
+                kwargss.append({'model_name':'dpformer'})
+                # for n_it in [3]:
+                #     kwargss.append({'model_name':'sinkformer','n_it':n_it,'is_op': is_op})
 
-        fix_embed = False
-        is_ops = [False, True]
-        #is_ops = [False]
-        qk_shares = [True, False]
-        #qk_shares = [False]
-                
-        #train_mask_type = 'longformer'
-        train_mask_type = None
+            common_kwargs = {                                  
+                "seed":              seed,
+                "qk_share":          qk_share,
+                'is_op':             is_op,
+                "is_rescale_dist":   is_rescale_dist,   
+                "is_resnet_scale":   is_resnet_scale,    
+                'lr_scheduler_type': lr_scheduler_type, 
+                'binary_ratio':      binary_ratio if lr_scheduler_type=='binary' else None,                                                                                                                        
+                "weight_decay":      0,
+                'max_lr':            1e-4,
+                'min_lr':            1e-5,
+                'n_layers':          n_layers,
+                'hidden':            hidden                            
+            }  
+            #common_kwargs['max_len'] = MAX_LENS_DICT[dataset_name] if max_len is None else max_len                        
+            common_kwargs['max_len'] = max_len
+            common_kwargs['fix_embed'] = fix_embed
+            if fix_embed:
+                common_kwargs["pretrained_model_name"] = pretrained_model_name
+            if train_mask_type is not None:
+                common_kwargs["train_mask_type"] = train_mask_type                
 
-        is_rescale_dist = True
-        #is_rescale_dist = False
-        is_resnet_scale = False
-
-        hidden = 256
-        max_len = 512
-
-        #is_train_others = True
-        is_train_others = False
-
-    model_sizes = [model_size]        
-    f_prefix = f'{n_layers}L-v4'
-    # if is_resnet_scale:
-    #     f_prefix += '-RR'
-    # if fix_embed:
-    #     ROOT = njoin(DROOT, f'{f_prefix}-hidden={hidden}-max_len={max_len}-{pretrained_model_name}') 
-    # else:
-    #     ROOT = njoin(DROOT, f'{f_prefix}-hidden={hidden}-max_len={max_len}')
-    # if train_mask_type is not None:
-    #     ROOT += f'-mask={train_mask_type}'        
-    # if is_rescale_dist:
-    #     ROOT += '-rescaled'        
-    ROOT = njoin(DROOT, 'test-run') 
-    job_path = njoin(ROOT, 'jobs_all')
-
-    for model_size in model_sizes:
-        kwargss_all = []    
-        for seed in seeds:
-            #for didx in [0]:
-            for didx in range(len(DATASET_NAMES)):
-
-                dataset_name = DATASET_NAMES[didx]
-                
-                # GPUs
-                select = 1; ngpus, ncpus = 1, 1
-                walltime = '23:59:59'
-                if model_size == 'large':
-                    mem = '10GB'  # 6L8H imdb (512 len)                      
+            if n_layers > 3:
+                common_kwargs['n_attn_heads'] = 8
+                common_kwargs["epochs"] = 20
+                common_kwargs["max_lr"] = 1e-4                            
+                common_kwargs["min_lr"] = 1e-5
+                common_kwargs["train_bs"] = 32
+            else:
+                common_kwargs['n_attn_heads'] = 1
+                if dataset_name.lower() in ['imdb', 'rotten_tomatoes', 'ag_news']:
+                    common_kwargs["epochs"] = 25
                 else:
-                    mem = '6GB'  # 1L1H imdb (512 len)                                                                                                       
+                    common_kwargs["epochs"] = 35                                                            
+                # common_kwargs["max_lr"] = 1e-3
+                # common_kwargs["min_lr"] = 1e-4
+                common_kwargs["max_lr"] = 1e-3
+                common_kwargs["min_lr"] = 2e-4
+                common_kwargs["train_bs"] = 16
 
-                for qk_share in qk_shares:
-                    for is_op in is_ops:
+            # ----- add more settings here -----                                           
+            model_root_dirname = structural_model_root(dataset_name=dataset_name, **common_kwargs)                                                  
+            model_root = njoin(ROOT, 'config_qqv' if qk_share else 'config_qkv', dataset_name, model_root_dirname)
+                
+            for idx in range(len(kwargss)):
+                # function automatically creates dir
+                kwargss[idx]["dataset"] = dataset_name    
+                kwargss[idx]['model_root'] = model_root
+            
+            kwargss = add_common_kwargs(kwargss, common_kwargs)
+            kwargss_all += kwargss
 
-                        kwargss = []
+    print(f'Total jobs: {len(kwargss_all)} \n')      
 
-                        for alpha in alphas:
-                            for bandwidth in bandwidths:
-                                for manifold in manifolds:
-                                    kwargss.append({'model_name':'fnsformer','alpha':alpha,'a': 0,'bandwidth':bandwidth,'manifold':manifold,
-                                                    'is_op': is_op})
+    batch_kwargss_all = []
+    kwargsss = [kwargss_all[i:i+nstack] for i in range(0, len(kwargss_all), nstack)]
+    for kwargss in kwargsss:
+        arg_strss = ''
+        for kwargs in kwargss:
+            arg_strss += ",".join("=".join((str(k),str(v))) for k,v in kwargs.items()) + ';'
+        batch_kwargss_all.append({'arg_strss': arg_strss[:-1]})
 
-                        if is_train_others:
-                            kwargss.append({'model_name':'dpformer','is_op': is_op})
+    print(f'Batched Total jobs: {len(batch_kwargss_all)} \n')
 
-                            # for n_it in [3]:
-                            #     kwargss.append({'model_name':'sinkformer','n_it':n_it,'is_op': is_op})
-
-                        common_kwargs = {                                  
-                            "seed":              seed,
-                            "qk_share":          qk_share,
-                            "is_rescale_dist":   is_rescale_dist,   
-                            "is_resnet_scale":   is_resnet_scale,                                                                                  
-                            #"lr_scheduler_type": "binary",                                       
-                            "weight_decay":      0,
-                            'max_lr':            1e-4,
-                            'min_lr':            1e-5                            
-                        }  
-                        #common_kwargs['max_len'] = MAX_LENS_DICT[dataset_name] if max_len is None else max_len                        
-                        common_kwargs['max_len'] = max_len
-
-                        #common_kwargs['n_attn_heads'] = 1 if is_op else 8
-                        #common_kwargs['n_attn_heads'] = 8
-     
-                        if model_size == 'large':
-                            common_kwargs["n_layers"] = n_layers
-                            common_kwargs['n_attn_heads'] = 8
-                            common_kwargs["hidden"] = hidden
-
-                            common_kwargs["epochs"] = 20
-                            #common_kwargs['fix_embed'] = False
-
-                            common_kwargs['lr_scheduler_type'] = 'binary'
-                            common_kwargs["max_lr"] = 1e-4                            
-                            common_kwargs["min_lr"] = 1e-5
-                            common_kwargs["train_bs"] = 32
-
-                        elif model_size == 'small':
-                            if fix_embed:
-                                common_kwargs["pretrained_model_name"] = pretrained_model_name
-
-                            common_kwargs["n_layers"] = n_layers
-                            common_kwargs['n_attn_heads'] = 1
-                            if dataset_name in ['imdb', 'rotten_tomatoes', 'ag_news']:
-                                common_kwargs["epochs"] = 25
-                            else:
-                                common_kwargs["epochs"] = 35
-                            common_kwargs['fix_embed'] = fix_embed                            
-
-                            # common_kwargs['lr_scheduler_type'] = 'constant'
-                            common_kwargs['lr_scheduler_type'] = 'binary'
-                            # common_kwargs["max_lr"] = 1e-3
-                            # common_kwargs["min_lr"] = 1e-4
-                            common_kwargs["max_lr"] = 1e-3
-                            common_kwargs["min_lr"] = 2e-4
-                            common_kwargs["train_bs"] = 16
-
-                            common_kwargs["hidden"] = hidden
-                            if train_mask_type is not None:
-                                common_kwargs["train_mask_type"] = train_mask_type
-
-                        # add more settings here                                           
-
-                        #use_custom_optim = False if 'use_custom_optim' not in common_kwargs.keys() else common_kwargs['use_custom_optim']                                                        
-                        model_root_dirname = structural_model_root(dataset_name=dataset_name, **common_kwargs)                          
-                        #model_root = njoin(ROOT, config_file.split('.')[0], dataset_name, model_root_dirname)
-                        model_root = njoin(ROOT, 'config_qqv' if qk_share else 'config_qkv', dataset_name, model_root_dirname)
-                            
-                        for idx in range(len(kwargss)):
-                            # function automatically creates dir
-                            kwargss[idx]["dataset"] = dataset_name    
-                            kwargss[idx]['model_root'] = model_root
-                        
-                        kwargss = add_common_kwargs(kwargss, common_kwargs)
-                        kwargss_all += kwargss
-
-        print(f'Total jobs: {len(kwargss_all)} \n')      
-
-        batch_kwargss_all = []
-        kwargsss = [kwargss_all[i:i+nstack] for i in range(0, len(kwargss_all), nstack)]
-        for kwargss in kwargsss:
-            arg_strss = ''
-            for kwargs in kwargss:
-                arg_strss += ",".join("=".join((str(k),str(v))) for k,v in kwargs.items()) + ';'
-            batch_kwargss_all.append({'arg_strss': arg_strss[:-1]})
-  
-        print(f'Batched Total jobs: {len(batch_kwargss_all)} \n')
-
-        commands, script_names, pbs_array_trues, kwargs_qsubs =\
-                job_setup(script_name, batch_kwargss_all,
-                        ncpus=ncpus,
-                        ngpus=ngpus,
-                        select=select, 
-                        walltime=walltime,
-                        mem=mem,
-                        job_path=job_path,
-                        nstack=nstack,
-                        cluster=CLUSTER)
-        
-        for i in range(len(commands)):
-            qsub(f'{commands[i]} {script_names[i]}', pbs_array_trues[i], path=job_path, **kwargs_qsubs[i])         
+    commands, script_names, pbs_array_trues, kwargs_qsubs =\
+            job_setup(script_name, batch_kwargss_all,
+                    q=q,
+                    ncpus=ncpus,
+                    ngpus=ngpus,
+                    select=select, 
+                    walltime=walltime,
+                    mem=mem,                    
+                    job_path=job_path,
+                    nstack=nstack,
+                    cluster=CLUSTER)
+    
+    for i in range(len(commands)):
+        qsub(f'{commands[i]} {script_names[i]}', pbs_array_trues[i], path=job_path, **kwargs_qsubs[i])         

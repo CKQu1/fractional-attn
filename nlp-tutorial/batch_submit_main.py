15a78a3358bf4c9245b0aa686df296f3a2355f51
K. Qu <kevinqu1995@hotmail.com>
2025-08-25 11:13:48 +1000

    only train models that do not have run_performance.csv

--- nlp-tutorial/batch_submit_main.py
+++ nlp-tutorial/batch_submit_main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from itertools import product
 from os.path import isfile, isdir
 from time import sleep
-from constants import DROOT, CLUSTER
+from constants import DROOT, CLUSTER, MODEL_SUFFIX
 from UTILS.mutils import njoin, get_seed, structural_model_root, str2bool
 from qsub_parser import job_setup, qsub, add_common_kwargs
 
@@ -16,7 +16,7 @@ if __name__ == '__main__':
           
     date_str = datetime.today().strftime('%Y-%m-%d')    
     script_name = "batch_main.py"
-    nstack = 9
+    nstack = 4
     MEM_DICT = {1: '4GB', 2: '6GB', 3: '8GB', 4: '10GB', 5: '12GB', 6: '14GB'}
     #CLUSTER = 'PHYSICS'  # can manually enter here too
     q = 'taiji'  # 'l40s', 'taiji'        
@@ -41,8 +41,10 @@ if __name__ == '__main__':
     if n_layers < 4:
         hiddens = [8, 16 ,32 ,64]
         #hiddens = [64]
+        n_attn_heads = 1
     else:                
-        hiddens = [256]     
+        hiddens = [256] 
+        n_attn_heads = 8    
     # training
     lr_scheduler_type = 'binary'  # 'constant'
     if lr_scheduler_type == 'binary':
@@ -55,13 +57,15 @@ if __name__ == '__main__':
     walltime = '23:59:59'
     mem = MEM_DICT[n_layers]   
 
+    # models
+    is_force_train = False
     # FNS
     alphas = [1, 1.2, 1.4, 1.6, 1.8, 2]  # 1, 1.2, 1.4, 1.6, 1.8, 2
     #alphas = [1]
     bandwidths = [1]  # 'median'      
     manifolds = ['rd']  # 'rd', 'v2_rd', 'sphere'       
     # other types
-    is_train_others = True               
+    is_train_others = True          
 
     kwargss_all = []    
     for seed, dataset_name in product(seeds, DATASET_NAMES):                                                                                                         
@@ -79,17 +83,6 @@ if __name__ == '__main__':
                 ROOT += f'-mask={train_mask_type}'        
             if is_rescale_dist:
                 ROOT += '-rescaled'                    
-
-            kwargss = []
-            # FNS
-            for alpha, bandwidth, manifold in product(alphas, bandwidths, manifolds):
-                kwargss.append({'model_name':'fnsformer','alpha':alpha,'a': 0,
-                                'bandwidth':bandwidth,'manifold':manifold})
-            # Other models
-            if is_train_others:
-                kwargss.append({'model_name':'dpformer'})
-                # for n_it in [3]:
-                #     kwargss.append({'model_name':'sinkformer','n_it':n_it,'is_op': is_op})
 
             common_kwargs = {                                  
                 "seed":              seed,
@@ -114,13 +107,13 @@ if __name__ == '__main__':
                 common_kwargs["train_mask_type"] = train_mask_type                
 
             if n_layers > 3:
-                common_kwargs['n_attn_heads'] = 8
+                common_kwargs['n_attn_heads'] = n_attn_heads
                 common_kwargs["epochs"] = 20
                 common_kwargs["max_lr"] = 1e-4                            
                 common_kwargs["min_lr"] = 1e-5
                 common_kwargs["train_bs"] = 32
             else:
-                common_kwargs['n_attn_heads'] = 1
+                common_kwargs['n_attn_heads'] = n_attn_heads
                 if dataset_name.lower() in ['imdb', 'rotten_tomatoes', 'ag_news']:
                     common_kwargs["epochs"] = 25
                 else:
@@ -134,7 +127,30 @@ if __name__ == '__main__':
             # ----- add more settings here -----                                           
             model_root_dirname = structural_model_root(dataset_name=dataset_name, **common_kwargs)                                                  
             model_root = njoin(ROOT, 'config_qqv' if qk_share else 'config_qkv', dataset_name, model_root_dirname)
-                
+
+            kwargss = []
+            qkv = 'qqv' if qk_share else 'qkv'
+            # FNS
+            for alpha, bandwidth, manifold in product(alphas, bandwidths, manifolds):
+                model_name = manifold + 'fns' +  MODEL_SUFFIX
+                model_name = 'op' + model_name if is_op else model_name
+                model_dir = njoin(model_root,
+                f'{model_name}-{dataset_name}-{qkv}-alpha={float(alpha)}-eps={float(bandwidth)}',
+                f'model={seed}')
+                if not isfile(njoin(model_dir, 'run_performance.csv')) or is_force_train:
+                    kwargss.append({'model_name':'fnsformer','alpha':alpha,'a': 0,
+                                    'bandwidth':bandwidth,'manifold':manifold})
+            
+            # Other models
+            if is_train_others:
+                model_name = 'dp' + MODEL_SUFFIX
+                model_name = 'op' + model_name if is_op else model_name
+                model_dir = njoin(model_root,f'{model_name}-{dataset_name}-{qkv}',f'model={seed}')                
+                if not isfile(njoin(model_dir, 'run_performance.csv')) or is_force_train:
+                    kwargss.append({'model_name':'dpformer'})
+                # for n_it in [3]:
+                #     kwargss.append({'model_name':'sinkformer','n_it':n_it,'is_op': is_op})              
+
             for idx in range(len(kwargss)):
                 # function automatically creates dir
                 kwargss[idx]["dataset"] = dataset_name    

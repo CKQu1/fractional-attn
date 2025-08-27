5af550388110643b1f8780691166d245356e0b9e
K. Qu <kevinqu1995@hotmail.com>
2025-08-27 11:26:24 +1000

    add arg script with default main.py

--- nlp-tutorial/batch_main.py
+++ nlp-tutorial/batch_main.py
@@ -1,14 +1,13 @@
 import argparse
 import os
 
-#from main import train_vit
-
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='nlp-tutorial/main.py training arguments')   
     # training settings 
     #parser.add_argument('--train_with_ddp', default=True, type=bool, help='to use DDP or not')
     parser.add_argument('--arg_strss', type=str)
+    parser.add_argument('--script', default='main.py', type=str)
 
     args = parser.parse_args()    
 
@@ -17,6 +16,4 @@ if __name__ == '__main__':
         #arg_strs = '--' + 'arg_strs.replace(',',' ')        
         arg_strs = '--' + ' --'.join(arg_strs.split(','))
 
-        os.system(f'python main.py {arg_strs}')        
-
-    
+        os.system(f'python {args.script} {arg_strs}')            

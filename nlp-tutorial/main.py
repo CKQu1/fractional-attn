80f4ffb612963ec6fcb1ca3bb6d993719d2ef619
Cheng Qu <cq5024@gadi-login-07.gadi.nci.org.au>
2025-09-08 12:28:14 +1000

    import and deploy load_dataset_and_tokenizer; add model.train()

--- nlp-tutorial/main.py
+++ nlp-tutorial/main.py
@@ -1,13 +1,8 @@
 import argparse
-from prenlp.tokenizer import NLTKMosesTokenizer
-from torch.utils.data import DataLoader
 
 from models.model_utils import create_longformer_mask
-from UTILS.data_utils import create_examples
-from tokenization import Tokenizer, PretrainedTokenizer
+from UTILS.dataloader import load_dataset_and_tokenizer
 #from trainer import Trainer
-
-TOKENIZER_CLASSES = {'nltk_moses': NLTKMosesTokenizer}
 
 import json
 import os
@@ -201,110 +196,10 @@ if __name__ == '__main__':
     device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
         
     #args.bandwidth = float(args.bandwidth) if args.bandwidth.replace('.','').isnumeric() else args.bandwidth
-    # poor man's data loader
-    if dataset_name == 'imdb':
-        if not args.fix_embed:
-            # Load tokenizer
-            if args.tokenizer_name == 'sentencepiece':
-                tokenizer = PretrainedTokenizer(pretrained_model=args.pretrained_model, vocab_file=args.vocab_file)
-            else:
-                tokenizer = TOKENIZER_CLASSES[args.tokenizer]()
-                tokenizer = Tokenizer(tokenizer=tokenizer, vocab_file =args.vocab_file)      
-
-        else:
-            if args.pretrained_model_name == 'distilbert-base-uncased':
-                from transformers import AutoTokenizer, DistilBertModel
-                #tokenizer = AutoTokenizer.from_pretrained(f'distilbert/{args.pretrained_model_name}')
-                tokenizer = AutoTokenizer.from_pretrained(f'distilbert/distilbert-base-uncased')
-                pretrained_model = DistilBertModel.from_pretrained("distilbert-base-uncased")
-                vocab_size, pretrained_model_hidden =\
-                     pretrained_model.embeddings.word_embeddings.weight.shape
-                pretrained_seq_len, _ = pretrained_model.embeddings.position_embeddings.weight.shape
-
-                assert args.max_len == pretrained_seq_len - 1, f'args.max_len does not match {pretrained_seq_len}!'
-
-            elif args.pretrained_model_name == 'albert-base-v2':
-                from transformers import AlbertTokenizer, AlbertModel
-                tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
-                pretrained_model = AlbertModel.from_pretrained("albert-base-v2")                
-                vocab_size, pretrained_model_hidden =\
-                     pretrained_model.embeddings.word_embeddings.weight.shape
-                pretrained_seq_len, _ = pretrained_model.embeddings.position_embeddings.weight.shape
-
-                assert args.max_len == pretrained_seq_len - 1, f'args.max_len does not match {pretrained_seq_len}!'
-
-            elif args.pretrained_model_name == 'gpt2':
-                from transformers import AutoModelForCausalLM, AutoTokenizer
-                tokenizer = AutoTokenizer.from_pretrained("gpt2")
-                pretrained_model = AutoModelForCausalLM.from_pretrained("gpt2")                                
-                vocab_size, pretrained_model_hidden =\
-                     pretrained_model.transformer.wte.weight.shape
-                pretrained_seq_len, _ = pretrained_model.transformer.wpe.weight.shape
-
-                assert args.max_len == pretrained_seq_len - 1, f'args.max_len does not match {pretrained_seq_len}!'
-
-            elif args.pretrained_model_name == 'glove':
-
-                from torchtext.data.utils import get_tokenizer
-                from torchtext.vocab import GloVe
-                from constants import GLOVE_DIMS
-                
-                if args.hidden in GLOVE_DIMS:
-                    print(f'Pretrained dimension {args.hidden} deployed! \n')
-
-                    # tokenizer = PretrainedTokenizer(pretrained_model=args.pretrained_model, 
-                    #                                 vocab_file=njoin(DROOT, 'GLOVE', f'vocab_npa_d={args.hidden}.npy'))
-                    tokenizer = get_tokenizer("basic_english")
-                    glove = GloVe(name='6B', dim=args.hidden)
-                    glove_dim = args.hidden
-                else:
-                    assert args.hidden < max(GLOVE_DIMS), 'Maximal glove dim is 300!'
-                    print(f'Reduced dimension {args.hidden} deployed! \n')
-
-                    # for glove_dim in GLOVE_DIMS:
-                    #     if glove_dim >= args.hidden:
-                    #         break
-                    glove_dim = 300
-                    tokenizer = get_tokenizer("basic_english")                    
-                    glove = GloVe(name='6B', dim=glove_dim)
-                vocab_size = len(glove.stoi)    
-
-            #max_length = tokenizer.model_max_length - 1
-            #max_length = tokenizer.model_max_length - 2
-            #args.max_len = tokenizer.model_max_length - 2
-            #args.max_len = tokenizer.model_max_length - 1
-
-            # def preprocess_function(examples):
-            #     return tokenizer(examples['text'], padding='max_length', truncation=True, max_length=max_length)
-
-        # Build DataLoader
-        if args.fix_embed and args.pretrained_model_name == 'glove':
-            from UTILS.data_utils import glove_create_examples
-            train_dataset = glove_create_examples(args, glove_dim, tokenizer, mode='train')
-            test_dataset = glove_create_examples(args, glove_dim, tokenizer, mode='test')            
-        else:
-            train_dataset = create_examples(args, tokenizer, mode='train')
-            test_dataset = create_examples(args, tokenizer, mode='test')
-        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)  
-
-        train_size = len(train_loader.dataset)
-        eval_size = len(test_loader.dataset)                      
-        steps_per_epoch = len(train_loader)        
-    else:
-        assert not args.fix_embed, f'fix_embed cannot be done for dataset {args.dataset_name}'
-        from transformers import AutoTokenizer
-        from UTILS.data_utils import get_datasets, datasets_create_examples
-
-        # Load a tokenizer (Example: BERT)
-        tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-        tokenized_train_dataset, tokenized_test_dataset = get_datasets(args, tokenizer)        
-        train_loader, test_loader = datasets_create_examples(args, 
-                                                             tokenized_train_dataset, 
-                                                             tokenized_test_dataset)        
-        train_size = len(tokenized_train_dataset)
-        eval_size = len(tokenized_test_dataset)  
-        steps_per_epoch = len(train_loader)
+    # ---------------------------------------- poor man's data loader ----------------------------------------
+    tokenizer, train_loader, test_loader, train_size, eval_size, steps_per_epoch, num_classes =\
+        load_dataset_and_tokenizer(args, batch_size)
+    # --------------------------------------------------------------------------------------------------------
 
     epochs = args.epochs
     if epochs is not None:  
@@ -346,7 +241,8 @@ if __name__ == '__main__':
         "is_op":    args.is_op,
         "fix_embed": args.fix_embed,
         "is_resnet_scale": args.is_resnet_scale,
-        "type_vocab_size": None        
+        "type_vocab_size": None,
+        "num_classes": num_classes        
     }
     config['device'] = device
 
@@ -357,12 +253,6 @@ if __name__ == '__main__':
             global_token_indices = [0,1]
             config['train_mask'] = create_longformer_mask(args.max_len, args.max_len//8, 
                                                           global_token_indices).bool().to(device) 
-
-    # dataset
-    if args.dataset_name == 'imdb':
-        config['num_classes'] = 2
-    else:
-        config['num_classes'] = len(tokenized_train_dataset['label'].unique())
 
     attn_setup = {'seed': args.seed,
                   'qk_share': args.qk_share, 'qkv_bias': args.qkv_bias, 
@@ -590,7 +480,8 @@ if __name__ == '__main__':
             epoch_val_accuracy += val_acc.item()
             epoch_val_loss += val_loss.item() 
         epoch_val_loss /= len(test_loader)
-        epoch_val_accuracy /= len(test_loader.dataset)             
+        epoch_val_accuracy /= len(test_loader.dataset)    
+        model.train()         
 
         return epoch_val_accuracy, epoch_val_loss
 
@@ -629,12 +520,14 @@ if __name__ == '__main__':
     train_n_batches, train_n_samples = len(train_loader), len(train_loader.dataset)    
 
     prev_lr = None
+
+    model.train()
     #for epoch in range(epochs):    
     for epoch in tqdm(range(epochs)):
         #print('epoch: ', epoch)
         epoch_loss = 0
         epoch_accuracy = 0
-
+        
         #for batch in tqdm(train_loader):
         for batch in train_loader:
 

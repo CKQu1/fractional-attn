10a104fe887b85bc4d17ea68726a7019c09d63c8
Cheng Qu <cq5024@gadi-login-07.gadi.nci.org.au>
2026-02-27 12:26:23 +1100

    Stop tracking folders/files unrelated to the paper

--- translation-large/quick_singularity_shell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-PBS_O_WORKDIR="/project/frac_attn/fractional-attn" 
-cpath="../../built_containers/FaContainer_v4.sif" 
-bpath1="/project"
-bpath2="/etc/localtime"
-#singularity shell -B ${bpath1} --home ${PBS_O_WORKDIR} ${cpath}
-singularity shell -B ${bpath1} ${cpath}
